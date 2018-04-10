df8619292e2771e5fe523e77d053839202a9ad9e
Shubham <shubhama@policybazaar.com>
2018-04-10 11:57:24 +0530

    api fixes

--- dev/js/api/api.js
+++ dev/js/api/api.js
@@ -4,18 +4,10 @@ let axiosInstance = Axios.create({
     baseURL: 'http://localhost:3000/api/',
 });
 
-export const API_GET = function(){
-    return axiosInstance.get
-}
+export const API_GET = axiosInstance.get
 
-export const API_POST = function(){
-    return axiosInstance.get
-}
+export const API_POST = axiosInstance.post
 
-export const API_PUT = function(){
-    return axiosInstance.put
-}
+export const API_PUT = axiosInstance.put
 
-export const API_DELETE = function(){
-    return axiosInstance.delete
-}
+export const API_DELETE = axiosInstance.delete
