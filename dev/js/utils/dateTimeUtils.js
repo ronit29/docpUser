ccd4c2bf0879c2113e8f11b38bfc83a791ae5298
rajivk <rajivk@policybazaar.com>
2018-04-12 15:40:18 +0530

    slot selector implemented

---
+++ dev/js/utils/dateTimeUtils.js
@@ -0,0 +1,12 @@
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+export const getTime = (timeStamp) =>  {
+    var date = new Date(timeStamp);
+    var hours = date.getHours();
+    var minutes = "0" + date.getMinutes();
+    return hours + ':' + minutes.substr(-2)
+}
+export const getDayName =(timeStamp) =>
+{
+   return days[new Date(timeStamp).getDay()]
+
+}
