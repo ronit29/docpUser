0c2be8573ca9bf7a2695f9af11ddb3ac60103453
Shubham <shubhama@policybazaar.com>
2018-08-09 14:53:16 +0530

    fix cookies

--- dev/js/helpers/storage/storage.js
+++ dev/js/helpers/storage/storage.js
@@ -1,6 +1,3 @@
-import Cookies from 'universal-cookie';
-const cookies = new Cookies();
-
 function deleteAllCookies() {
     var cookies = document.cookie.split(";");
 
@@ -11,21 +8,42 @@ function deleteAllCookies() {
         document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
     }
 }
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {
+    document.cookie = name + '=; Max-Age=-99999999;';
+}
 
 const STORAGE = {
     setAuthToken: (token) => {
-        cookies.set('tokenauth', token, { path: '/' })
+        setCookie('tokenauth', token, 10)
         return Promise.resolve(true)
     },
     getAuthToken: () => {
-        return Promise.resolve(cookies.get('tokenauth'))
+        return Promise.resolve(getCookie('tokenauth'))
     },
     checkAuth: () => {
-        return !!cookies.get('tokenauth')
+        return !!getCookie('tokenauth')
     },
     deleteAuth: () => {
-        cookies.remove('tokenauth', { path: '/' })
-        cookies.remove('tokenauth')
+        eraseCookie('tokenauth')
         deleteAllCookies()
         return Promise.resolve()
     }
