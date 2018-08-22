d9f701f720cc81d22dbd4e73698de12cb0b04675
Kunal Batra <>
2018-08-22 13:01:50 +0530

    design fixes

--- dev/js/components/opd/doctorProfile/DoctorProfileView.js
+++ dev/js/components/opd/doctorProfile/DoctorProfileView.js
@@ -65,7 +65,7 @@ class DoctorProfileView extends React.Component {
                                             </div>
                                             {/* <ul className="inline-list float-right user-notification-action">
                                                 <li><span className="icon icon-md text-middle"><img src={ASSETS_BASE_URL + "/img/customer-icons/user.svg"} className="img-fluid" /></span></li>
-                                    <li><span className="icon icon-md text-middle notification-icon"><img src={ASSETS_BASE_URL + "/img/customer-icons/notification.svg"} className="img-fluid" /> <span className="notification-alert" /></span></li> 
+                                                <li><span className="icon icon-md text-middle notification-icon"><img src={ASSETS_BASE_URL + "/img/customer-icons/notification.svg"} className="img-fluid" /> <span className="notification-alert" /></span></li> 
                                             </ul> */}
                                         </div>
                                     </div>
