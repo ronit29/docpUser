1c9785607d5d3f7885947d0cb921b4b9fe1266df
Shubham <shubhama@policybazaar.com>
2018-08-23 17:49:46 +0530

    adress fix

--- dev/js/components/commons/userProfile/userAddress/add/addAddress.js
+++ dev/js/components/commons/userProfile/userAddress/add/addAddress.js
@@ -14,9 +14,30 @@ class UserSignupView extends React.Component {
             address: '',
             land_mark: '',
             pincode: '',
+            locality: '',
             type: 'home',
             phone_number: def_profile ? def_profile.phone_number : "",
-            edit: !!this.props.match.params.id
+            edit: !!this.props.match.params.id,
+            land_mark_results: [],
+            locality_results: []
+        }
+    }
+
+    getLocation(location, resultField) {
+        var auto = new google.maps.places.AutocompleteService()
+
+        var request = {
+            input: location,
+            types: ['(regions)'],
+            componentRestrictions: { country: 'in' }
+        };
+        if (location) {
+            auto.getPlacePredictions(request, function (results, status) {
+                results = results || []
+                this.setState({ [resultField]: results })
+            }.bind(this))
+        } else {
+            this.setState({ [resultField]: [] })
         }
     }
 
@@ -50,6 +71,14 @@ class UserSignupView extends React.Component {
 
     inputHandler(e) {
         this.setState({ [e.target.name]: e.target.value })
+
+        if (e.target.name == 'land_mark') {
+            this.getLocation(e.target.value, 'land_mark_results')
+        }
+
+        if (e.target.name == 'locality') {
+            this.getLocation(e.target.value, 'locality_results')
+        }
     }
 
     submitForm() {
@@ -138,8 +167,26 @@ class UserSignupView extends React.Component {
                                     <span className="text-xs"> (will be used at the time of sample pickup)</span>
                                 </div>
                                 <div className="labelWrap">
-                                    <input id="locality" name="lname" type="text" required />
+                                    <input id="locality" name="locality" type="text" onChange={this.inputHandler.bind(this)} value={this.state.locality} required />
                                     <label htmlFor="locality">Select Locality</label>
+
+                                    {
+                                        this.state.locality_results.length ? <div className="panel-content pd-0">
+                                            <ul className="list city-list">
+                                                {
+                                                    this.state.locality_results.map((result, i) => {
+                                                        return <li key={i}>
+                                                            <a>{result.description}
+                                                                <span className="city-loc">City</span>
+                                                            </a>
+                                                        </li>
+                                                    })
+                                                }
+                                            </ul>
+                                        </div> : ""
+                                    }
+
+
                                 </div>
                                 <div className="labelWrap">
                                     <input id="address" name="address" type="text" onChange={this.inputHandler.bind(this)} value={this.state.address} required ref="address" />
@@ -148,6 +195,24 @@ class UserSignupView extends React.Component {
                                 <div className="labelWrap">
                                     <input id="land_mark" name="land_mark" type="text" onChange={this.inputHandler.bind(this)} value={this.state.land_mark} required ref="land_mark" />
                                     <label htmlFor="land_mark">Land Mark</label>
+
+                                    {
+                                        this.state.land_mark_results.length ? <div className="panel-content pd-0">
+                                            <ul className="list city-list">
+                                                {
+                                                    this.state.land_mark_results.map((result, i) => {
+                                                        return <li key={i}>
+                                                            <a>{result.description}
+                                                                <span className="city-loc">City</span>
+                                                            </a>
+                                                        </li>
+                                                    })
+                                                }
+                                            </ul>
+                                        </div> : ""
+                                    }
+
+
                                 </div>
                                 <div className="labelWrap">
                                     <input id="pincode" name="pincode" type="text" onChange={this.inputHandler.bind(this)} value={this.state.pincode} required ref="pincode" />
