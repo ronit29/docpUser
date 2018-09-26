cd186525a39a3a1518eea8147320d4539e3c6eba
Kunal Batra <>
2018-09-26 19:08:36 +0530

    how it works

--- dev/js/components/commons/staticPages/howitWorks.js
+++ dev/js/components/commons/staticPages/howitWorks.js
@@ -17,7 +17,8 @@ class HowitWorks extends React.Component {
                         <p className="fw-500 about-heading">How it Works</p>
                     </div>
                     <div className="col-12">
-                        <p className="fw-500 about-content">DocPrime aims to provide efficient and affordable access to doctors and medical help to people across India and improving the healthcare experience for all humanity.</p>
+                        <p className="fw-500 about-content">docprime.com aims to redefine how Indians seek healthcare services. It connects patients with medical consultants in real time and bridges the gap between need and fulfilment using state-of-the-art technology and a robust offline network. It also facilitates booking of doctor appointments and lab tests at discounted rates.</p>
+                        <p className="fw-500 about-content">Our Key Services are:</p>
                     </div>
                 </div>
                 <div className="row working-row">
@@ -27,13 +28,15 @@ class HowitWorks extends React.Component {
                                 <p className="fw-500">1</p>
                             </div>
                             <div className="consultation-text">
-                                <p className="fw-500">Doctor Consultation</p>
+                                <p className="fw-500">Free for Life-Doctor consultation</p>
                             </div>
                         </div>
                         <div className="doctext">
-                            <p className="fw-500">
-                                Our flawless platform helps user get answers to their queries and offers one-on-one interaction with experienced doctors for free. Users can anonymously, or otherwise, communicate with doctors with various areas of expertise directly from their smartphones.
-                            </p>
+                            <ul style={{ listStyleType: 'disc' }}>
+                                <li className="fw-500">Free medical consultation over chat and phone from experienced medical consultants</li>
+                                <li className="fw-500">Instant and real-time interaction with medical consultants to help identify the root cause of the problem</li>
+                                <li className="fw-500">Users can anonymously, or otherwise, communicate with doctors with various areas of expertise directly from their smartphones or desktops</li>
+                            </ul>
                         </div>
                     </div>
                     <div className="col-lg-4 d-none d-lg-block">
@@ -47,13 +50,15 @@ class HowitWorks extends React.Component {
                                 <p className="fw-500">2</p>
                             </div>
                             <div className="consultation-text">
-                                <p className="fw-500">Lab Tests</p>
+                                <p className="fw-500">Doctor Search and Online Appointment Booking</p>
                             </div>
                         </div>
                         <div className="doctext">
-                            <p className="fw-500">
-                                Detailed information about procedures of the tests, prices, and relevant preparations are easily accessible on our platform. This is to ensure that in the need of the hour, a disease can be diagnosed without having to leave the house.
-                            </p>
+                            <ul style={{ listStyleType: 'disc' }}>
+                                <li className="fw-500">Search the best doctors nearby your area and book your next doctor visit conveniently through us</li>
+                                <li className="fw-500">Avail discount upto 50% on booking doctor services</li>
+                                <li className="fw-500">14000+doctors from reputed and leading clinics and hospitals on board to provide best healthcare services</li>
+                            </ul>
                         </div>
                     </div>
                     <div className="col-lg-4 d-none d-lg-block">
@@ -67,17 +72,81 @@ class HowitWorks extends React.Component {
                                 <p className="fw-500">3</p>
                             </div>
                             <div className="consultation-text">
-                                <p className="fw-500">Staying Healthy</p>
+                                <p className="fw-500">Search and Book Lab Tests at Discounted Rates</p>
                             </div>
                         </div>
                         <div className="doctext">
-                            <p className="fw-500">
-                                The feed on the website is created with the aim of keeping people updated of the latest development in the healthcare industry. This feed is curated by healthcare experts and is filled with information that can help users make an informed decision about their health.
-                            </p>
+                            <ul style={{ listStyleType: 'disc' }}>
+                                <li className="fw-500">Search the best diagnostic lab nearby your area</li>
+                                <li className="fw-500">Avail discount upto 60% on lab tests</li>
+                                <li className="fw-500">Comprehensive network of reputed 4000+ diagnostic labs</li>
+                                <li className="fw-500">Detailed information about procedures of the tests, prices, and relevant preparations are easily accessible on the platform</li>
+                                <li className="fw-500">Home collection facility available</li>
+                            </ul>
                         </div>
                     </div>
                     <div className="col-lg-4 d-none d-lg-block">
                         <img className="consultation-image" src={ASSETS_BASE_URL + "/img/stayinghealthy.png"} />
+                    </div>
+                </div>
+                <div className="row working-row lab">
+                    <div className="working-content-div col-12 col-lg-8">
+                        <div className="doctor-consultation">
+                            <div className="working-count text-center">
+                                <p className="fw-500">4</p>
+                            </div>
+                            <div className="consultation-text">
+                                <p className="fw-500">Health feed</p>
+                            </div>
+                        </div>
+                        <div className="doctext">
+                            <ul style={{ listStyleType: 'disc' }}>
+                                <li className="fw-500">Important facts and knowledge about various diseases and medicines, and how to manage the condition</li>
+                                <li className="fw-500">Information on useful lifestyle changes for overall well-being</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="row lab">
+                    <div className="working-content-div col-12 col-lg-8">
+                        <p className="fw-500 text-xl">Our Upcoming Services :</p>
+                    </div>
+                </div>
+                <div className="row working-row lab" style={{marginTop: 40}} >
+                    <div className="working-content-div col-12 col-lg-8">
+                        <div className="doctor-consultation">
+                            <div className="working-count text-center">
+                                <p className="fw-500">1</p>
+                            </div>
+                            <div className="consultation-text">
+                                <p className="fw-500">Subscription based OPD product</p>
+                            </div>
+                        </div>
+                        <div className="doctext">
+                            <ul style={{ listStyleType: 'disc' }}>
+                                <li className="fw-500">Unlimited consultations and diagnostic test to make regular OPD visits convenient, accessible, and affordable</li>
+                                <li className="fw-500">Ease of cashless transactions for OPD services</li>
+                                <li className="fw-500">Online appointment booking</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="row working-row lab">
+                    <div className="working-content-div col-12 col-lg-8">
+                        <div className="doctor-consultation">
+                            <div className="working-count text-center">
+                                <p className="fw-500">2</p>
+                            </div>
+                            <div className="consultation-text">
+                                <p className="fw-500">ePharmacy</p>
+                            </div>
+                        </div>
+                        <div className="doctext">
+                            <ul style={{ listStyleType: 'disc' }}>
+                                <li className="fw-500">Comprehensive network of pharmacies</li>
+                                <li className="fw-500">Doorstep delivery of medicines at discounted rates</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
