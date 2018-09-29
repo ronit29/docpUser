1eba2b0cd42e081b8666c4d3c8646bdca198907b
shubhamqweasd <shubhamqweasd@gmail.com>
2018-09-29 09:46:10 +0530

    read files async

--- index.js
+++ index.js
@@ -44,146 +44,151 @@ app.all('*', function (req, res) {
     /**
      * Fetch Css files
      */
-    let files = fs.readdirSync(DIST_FOLDER)
-    let css_file = null
-    for (let file of files) {
-        if (file.includes('.css')) {
-            css_file = fs.readFileSync(`${DIST_FOLDER}${file}`, 'utf-8')
-        }
-    }
-    let bootstrap_file = fs.readFileSync(`./assets/css/bootstrap-grid.min.css`, 'utf-8')
-    /** 
-     *  Track API calls for funneling 
-     */
-    let ip = req.headers['x-forwarded-for'] ||
-        req.connection.remoteAddress ||
-        req.socket.remoteAddress ||
-        (req.connection.socket ? req.connection.socket.remoteAddress : null);
+    // let files = fs.readdirSync(DIST_FOLDER)
+    // let css_file = null
+    // for (let file of files) {
+    //     if (file.includes('.css')) {
+    //         css_file = fs.readFileSync(`${DIST_FOLDER}${file}`, 'utf-8')
+    //     }
+    // }
+    // let bootstrap_file = fs.readFileSync(`./assets/css/bootstrap-grid.min.css`, 'utf-8')
 
-    axios.post(CONFIG.API_BASE_URL + '/api/v1/tracking/serverhit', {
-        url: req.url,
-        refferar: req.headers.referer,
-        ip: ip,
-        type: 'server'
-    }).then((res) => {
-        // console.log(res)
-    }).catch((e) => {
-        // console.log(e)
-    })
+    _readStyles().then((styleFiles) => {
+        
+        let css_file = styleFiles[0]
+        let bootstrap_file = styleFiles[1]
 
-    /**
-     * Initialized store with persisted reducer and all middlewares
-     * TODO: use persisted data for inital render
-     */
-    const store = createStore(
-        allReducers, applyMiddleware(thunk)
-    );
-    /** 
-     * Check if a route is enabled for SSR , RENDER_ON_SERVER == true,
-     * if enabled, check if it needs any data(async API) before rendering, if so
-     * then wait for that data to resolve then render with proper data.
-     */
-    const promises = []
-    Routes.ROUTES.some(route => {
-        // use `matchPath` here
-        const match = matchPath(req.path, route)
-        if (match && route.RENDER_ON_SERVER) {
-            if (route.component.loadData) {
-                promises.push(route.component.loadData(store, match, req.query))
-            } else {
-                promises.push(Promise.resolve({}))
+        /** 
+         *  Track API calls for funneling 
+         */
+        let ip = req.headers['x-forwarded-for'] ||
+            req.connection.remoteAddress ||
+            req.socket.remoteAddress ||
+            (req.connection.socket ? req.connection.socket.remoteAddress : null);
+
+        axios.post(CONFIG.API_BASE_URL + '/api/v1/tracking/serverhit', {
+            url: req.url,
+            refferar: req.headers.referer,
+            ip: ip,
+            type: 'server'
+        }).then((res) => {
+            // console.log(res)
+        }).catch((e) => {
+            // console.log(e)
+        })
+
+        /**
+         * Initialized store with persisted reducer and all middlewares
+         * TODO: use persisted data for inital render
+         */
+        const store = createStore(
+            allReducers, applyMiddleware(thunk)
+        );
+        /** 
+         * Check if a route is enabled for SSR , RENDER_ON_SERVER == true,
+         * if enabled, check if it needs any data(async API) before rendering, if so
+         * then wait for that data to resolve then render with proper data.
+         */
+        const promises = []
+        Routes.ROUTES.some(route => {
+            // use `matchPath` here
+            const match = matchPath(req.path, route)
+            if (match && route.RENDER_ON_SERVER) {
+                if (route.component.loadData) {
+                    promises.push(route.component.loadData(store, match, req.query))
+                } else {
+                    promises.push(Promise.resolve({}))
+                }
             }
-        }
-        return match
-    })
+            return match
+        })
 
-    /** 
-     * Only when a route matches all criteria for SSR, we do SSR
-     */
-    if (promises && promises.length) {
+        /** 
+         * Only when a route matches all criteria for SSR, we do SSR
+         */
+        if (promises && promises.length) {
 
-        // set a timeout to check if SSR is taking too long, if it does , just render the normal page.
-        let SSR_TIMER = setTimeout(() => {
+            // set a timeout to check if SSR is taking too long, if it does , just render the normal page.
+            let SSR_TIMER = setTimeout(() => {
+                res.render('index.ejs', {
+                    html: "", storeData: "{}", helmet: null, ASSETS_BASE_URL: ASSETS_BASE_URL, css_file, bootstrap_file
+                })
+            }, 5000)
+
+            Promise.all(promises).then(data => {
+                try {
+                    /**
+                     * Context for async data loading -> mimic componentDidMount actions.
+                     */
+                    let context = {}
+                    if (data && data[0]) {
+                        context.data = data[0]
+                    }
+
+                    const storeData = JSON.stringify(store.getState())
+                    const html = ReactDOMServer.renderToString(
+                        <Provider store={store}>
+                            <div>
+                                <StaticRouter
+                                    location={req.url}
+                                    context={context}
+                                >
+                                    <div>
+                                        <Routes />
+                                    </div>
+                                </StaticRouter>
+                            </div>
+                        </Provider>
+                    )
+                    const helmet = Helmet.renderStatic()
+
+                    // clear timer to mark success in SSR
+                    clearTimeout(SSR_TIMER)
+
+                    res.render('index.ejs', {
+                        html, storeData, helmet, ASSETS_BASE_URL: ASSETS_BASE_URL, css_file, bootstrap_file
+                    })
+
+                } catch (e) {
+
+                    if (CONFIG.RAVEN_SERVER_DSN_KEY) {
+                        Sentry.captureException(e)
+                    }
+
+                    res.render('index.ejs', {
+                        html: "", storeData: "{}", helmet: null, ASSETS_BASE_URL: ASSETS_BASE_URL, css_file, bootstrap_file
+                    })
+                }
+
+            }).catch((error) => {
+                /** 
+                 * If a new url is sent via any API call, then redirect client.
+                 */
+                if (error && error.url) {
+                    res.redirect(`/${error.url}`);
+                } else {
+
+                    if (CONFIG.RAVEN_SERVER_DSN_KEY) {
+                        Sentry.captureException(error)
+                    }
+
+                    res.render('index.ejs', {
+                        html: "", storeData: "{}", helmet: null, ASSETS_BASE_URL: ASSETS_BASE_URL, css_file, bootstrap_file
+                    })
+                }
+            })
+        } else {
             res.render('index.ejs', {
                 html: "", storeData: "{}", helmet: null, ASSETS_BASE_URL: ASSETS_BASE_URL, css_file, bootstrap_file
             })
-        }, 5000)
+        }
 
-        Promise.all(promises).then(data => {
-            try {
-                /**
-                 * Context for async data loading -> mimic componentDidMount actions.
-                 */
-                let context = {}
-                if (data && data[0]) {
-                    context.data = data[0]
-                }
+    }).catch((e) => {
 
-                const storeData = JSON.stringify(store.getState())
-                const html = ReactDOMServer.renderToString(
-                    <Provider store={store}>
-                        <div>
-                            <StaticRouter
-                                location={req.url}
-                                context={context}
-                            >
-                                <div>
-                                    <Routes />
-                                </div>
-                            </StaticRouter>
-                        </div>
-                    </Provider>
-                )
-                const helmet = Helmet.renderStatic()
-
-                // clear timer to mark success in SSR
-                clearTimeout(SSR_TIMER)
-
-                res.render('index.ejs', {
-                    html, storeData, helmet, ASSETS_BASE_URL: ASSETS_BASE_URL, css_file, bootstrap_file
-                })
-
-            } catch (e) {
-
-                if (CONFIG.RAVEN_SERVER_DSN_KEY) {
-                    Sentry.captureException(e)
-                }
-
-                res.render('index.ejs', {
-                    html: "", storeData: "{}", helmet: null, ASSETS_BASE_URL: ASSETS_BASE_URL, css_file, bootstrap_file
-                })
-            }
-
-        }).catch((error) => {
-            /** 
-             * If a new url is sent via any API call, then redirect client.
-             */
-            if (error && error.url) {
-                res.redirect(`/${error.url}`);
-            } else {
-
-                if (CONFIG.RAVEN_SERVER_DSN_KEY) {
-                    Sentry.captureException(error)
-                }
-
-                res.render('index.ejs', {
-                    html: "", storeData: "{}", helmet: null, ASSETS_BASE_URL: ASSETS_BASE_URL, css_file, bootstrap_file
-                })
-            }
-        })
-    } else {
-        res.render('index.ejs', {
-            html: "", storeData: "{}", helmet: null, ASSETS_BASE_URL: ASSETS_BASE_URL, css_file, bootstrap_file
-        })
-    }
-
+    })
 
 });
 
-
-// app.use(function (req, res) {
-//     res.sendFile('index.html', { root: './dist/' })
-// })
 
 if (CONFIG.RAVEN_SERVER_DSN_KEY) {
     app.use(Sentry.Handlers.errorHandler())
@@ -195,3 +200,37 @@ server.listen(process.env.PORT || 3000, (err) => {
     }
     console.info(`Server running on http://localhost:${process.env.PORT || 3000}`);
 });
+
+
+function _readStyles() {
+    return new Promise((resolve, reject) => {
+        fs.readdir(DIST_FOLDER, (err, files) => {
+            let stylePromises = []
+            for (let file of files) {
+                if (file.includes('.css')) {
+                    stylePromises.push(_readFileAsync(`${DIST_FOLDER}${file}`))
+                    break
+                }
+            }
+            stylePromises.push(_readFileAsync(`./assets/css/bootstrap-grid.min.css`))
+
+            Promise.all(stylePromises).then((styleFilesData) => {
+                resolve(styleFilesData)
+            }).catch((e) => {
+                reject(e)
+            })
+        })
+    })
+}
+
+function _readFileAsync(filename) {
+    return new Promise((resolve, reject) => {
+        try {
+            fs.readFile(filename, 'utf-8', (err, data) => {
+                if (err) reject(err); else resolve(data);
+            })
+        } catch (err) {
+            reject(err)
+        }
+    });
+};
