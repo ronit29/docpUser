f214526faaf026a2573f4e410abe36536c6bedf6
Mansi Suneja <mansis@policybazaar.com>
2018-10-04 17:39:30 +0530

    Doctor and Lab list ad

--- dev/js/components/diagnosis/searchResults/labsList/LabsList.js
+++ dev/js/components/diagnosis/searchResults/labsList/LabsList.js
@@ -130,11 +130,29 @@ class LabsList extends React.Component {
                                     >
                                         {
                                             labList.map((labId, i) => {
+                                                if(i == 1){
+                                                    return <div className="no-risk-container mt-3">
+                                                     <div className="no-rsk">
+                                                         <div className="rsk-image">
+                                                             <img className="" src={ASSETS_BASE_URL + "/img/customer-icons/group-98.png"} />
+                                                         </div>
+                                                         <div className="rsk-content">
+                                                             <h4 className="rsk-hdng">Amazing Savings... No Risks!</h4>
+                                                             <ul className="rsk-lstng ff">
+                                                                 <li className="lst-bfr">Upto 50% Off on doctor and lab bookings</li>
+                                                                 <li className="lst-bfr">100% money back guarantee -  No questions!</li>
+                                                             </ul>
+                                                         </div>
+                                                     </div>
+                                                 </div>
+                                 
+                                                 }else{
                                                 if (LABS[labId]) {
                                                     return <LabProfileCard {...this.props} details={LABS[labId]} key={i} rank={i} />
                                                 } else {
                                                     return ""
                                                 }
+                                            }
                                             })
                                         }
                                     </InfiniteScroll>
