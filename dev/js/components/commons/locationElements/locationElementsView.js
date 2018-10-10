f728572778ba8009a1ad788e5ade652cb1fb5b85
Kunal Batra <>
2018-10-10 12:54:51 +0530

    design fix

--- dev/js/components/commons/locationElements/locationElementsView.js
+++ dev/js/components/commons/locationElements/locationElementsView.js
@@ -167,7 +167,7 @@ class LocationElementsView extends React.Component {
 
         return (
             // toggle class : 'doc-select-none'
-            <div className="row" style={{ backgroundColor: '#f78316', marginTop: 10 }}>
+            <div className="row" style={{ backgroundColor: '#f78316', marginTop: 10, position: 'relative', zIndex: 11 }}>
 
                 <div className="col-12">
                     {
