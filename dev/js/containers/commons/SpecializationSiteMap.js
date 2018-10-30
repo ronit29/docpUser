49e377c7da1d1b3cf46f94bf8c42c49caad7da1c
shubhamqweasd <shubhamqweasd@gmail.com>
2018-10-30 13:22:25 +0530

    fix

--- dev/js/containers/commons/SpecializationSiteMap.js
+++ dev/js/containers/commons/SpecializationSiteMap.js
@@ -1,28 +1,32 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import SpecializationMapRoutes from '../../components/commons/SpecializationMapRoutes'
-import {  getSpecialitiesMap } from '../../actions/index.js'
+import { setFetchResults, getSpecialitiesMap } from '../../actions/index.js'
 
 class SpecializationMap extends React.Component {
 
 	static loadData(store, match, query) {
-        if(match.params.speciality){
-            return store.dispatch(getSpecialitiesMap(match.params.speciality, query.page))
-        }else{
-            return store.dispatch(getSpecialitiesMap())
-        }
-        
-    }
+		if (match.params.speciality) {
+			return store.dispatch(getSpecialitiesMap(match.params.speciality, query.page))
+		} else {
+			return store.dispatch(getSpecialitiesMap())
+		}
 
-    static contextTypes = {
-        router: () => null
-    }
+	}
 
-	render(){
-		
-		return(
+	static contextTypes = {
+		router: () => null
+	}
+
+	componentDidMount() {
+		this.props.setFetchResults(true)
+	}
+
+	render() {
+
+		return (
 			<SpecializationMapRoutes {...this.props} />
-			)
+		)
 	}
 }
 
@@ -32,7 +36,7 @@ const mapStateToProps = (state) => {
 		specialitiesMapCities
 	} = state.SITE_MAP
 
-	return{
+	return {
 		specialitiesMap,
 		specialitiesMapCities
 	}
@@ -41,10 +45,11 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
 
-	return{
-		getSpecialitiesMap: (speciality, page) => dispatch(getSpecialitiesMap(speciality, page))
+	return {
+		getSpecialitiesMap: (speciality, page) => dispatch(getSpecialitiesMap(speciality, page)),
+		setFetchResults: (status) => dispatch(setFetchResults(status))
 	}
 
 }
 
-export default connect(mapStateToProps, mapDispatchToProps) (SpecializationMap)
+export default connect(mapStateToProps, mapDispatchToProps)(SpecializationMap)
