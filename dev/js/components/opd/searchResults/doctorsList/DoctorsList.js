2cf883b7b29c549d52fcd124aa655b5460315d48
prince kumar <princek@policybazaar.com>
2018-11-02 18:14:16 +0530

    search content added

--- dev/js/components/opd/searchResults/doctorsList/DoctorsList.js
+++ dev/js/components/opd/searchResults/doctorsList/DoctorsList.js
@@ -14,7 +14,8 @@ class DoctorsList extends React.Component {
             hasMore: true,
             loading: false,
             renderBlock: false,
-            page: 1
+            page: 1,
+            readMore: 'search-details-data-less'
         }
     }
 
@@ -81,26 +82,25 @@ class DoctorsList extends React.Component {
                 {
                     this.state.renderBlock ? <Loader /> :
                         <div className="container-fluid">
-                            <div className="search-result-card-collpase">
-                                <div className="search-details-data-less">
-                                <h5>Anaesthesiologists in Chennai </h5>
-                                <p>Anaesthesiologists in Chennai are acclaimed for taking care of patients during and post-surgery so that the procedure goes well without being felt by the patients, in and around Chennai, prevent any glitch in between the surgery. The best Anaesthesiologists in Chennai are always ready to cater to the needs of their patients.</p>
-                                <h5>When should you Book Appointments with Anaesthesiologists in Chennai?</h5>
-                                <p>An Anaesthesiologists is a health professional that has been trained in providing enough amount of anaesthesia required by the patients to undergo the surgery. The field specializes in diagnosing, identifying, and successfully giving anaesthesia to the patients. Booking appointments with Anaesthesiologists in Chennai helps in the following manner: Anaesthesiologists help the patients by providing them anaesthesia and protect them from any ill effects of anaesthesia during and after surgery. Anaesthesia gives the best possible relief from all kind of surgical pain.The benefits are not just limited to patients but also enable doctors to perform surgery without any glitch and provide them comfortable working conditions. </p>
-                                <p>And this is not it. At docprime, you can search for a health expert and certified Anaesthesiologists across all different locations in Chennai.
-                                Why Should You Book Online Anaesthesiologists Appointments in Chennai?
-                                </p>
-                                <p>Ensures Your Comfort:
-                                When you are looking for experienced Anaesthesiologists in Chennai, booking appointments online can be done right on your hands via the mobile application which saves your time and energy. This gives you more convenience when you are suffering from a disease or illness. It simply saves energy and time that would have been lost while you travel to and fro from the hospital or clinic simply to get an appointment. </p>
-                                <p>Educates You And Your Family:
-                                At docprime, you can not only find the best and most experienced Anaesthesiologists in Chennai but also learn about the causes, symptoms, treatments, precautions, and other aspects of the concerned health condition in the comfort of your home.</p>
-                                <p>Attends Elderly:
-                                Often, aged individuals need medical attention and they find it difficult to commute to the clinics in Chennai simply to get an appointment for a later date. Now book appointments with a doctor right from your home with just a few clicks of your mouse and let them address the health concerns of your beloved elderly family members.</p>
-                                
-                                </div>
-                                <span className="rd-more">Read More</span>
+                            {
+                                this.props.search_content && this.props.search_content !=''?
+                                <div className="search-result-card-collpase">
+                                    <div className={this.state.readMore} dangerouslySetInnerHTML={{ __html: this.props.search_content }} >
+                                    </div>
+                                    
+                                    {   this.state.readMore && this.state.readMore != ''?
+                                        <span className="rd-more" onClick={()=>this.setState({readMore:''})}>Read More</span>
+                                        :''
+                                    }
 
-                            </div>
+                                    {   this.state.readMore == ''?
+                                        <span className="rd-more" onClick={()=>this.setState({readMore:'search-details-data-less'})}>Read Less</span>
+                                        :''
+                                    }
+
+                                </div>
+                                :''
+                            }
                             <div className="row">
                                 <div className="col-12">
                                     <InfiniteScroll
