c909cf74c02adfcc63c830c17c636a09b4a2b5a0
prince kumar <princek@policybazaar.com>
2018-12-07 17:29:48 +0530

    done

--- dev/js/components/diagnosis/commons/labTests/packageTest.js
+++ dev/js/components/diagnosis/commons/labTests/packageTest.js
@@ -18,18 +18,30 @@ class PackageTest extends React.Component {
         let { deal_price, mrp, pre_test_info } = test
         let test_package = test.package || []
         return (
-            <li style={{paddingRight: '0px'}} className="clearfix" key={i}>
-                <label className="ck-bx" style={{fontWeight: '400', fontSize: '14px'}} onClick={() => this.packageNameClick()}>
-                <p style={{paddingRight: '120px'}}>
-                {test.test.name}
-                    <button className="pkg-info-btn" onClick={() => { toggle('showPackageInfo', test) }}>
-                        <img src={ASSETS_BASE_URL + "/img/customer-icons/info.svg"} />
-                    </button>
-                </p>
-                <input type="checkbox" value="on" checked={this.props.test.is_selected ? true : false} onChange={this.props.toggleTest.bind(this, test)}/>
-                    <span className="checkmark">
-                    </span>
+            <li key={i} style={{paddingRight: '0px'}} className="clearfix" key={i}>
+                <label className="ck-bx" style={{fontWeight: '400', fontSize: '14px'}} >
+                    <p style={{paddingRight: '120px'}} onClick={(e) => {
+                        this.props.toggleTest.bind(this, test)
+                        e.preventDefault()
+                        e.stopPropagation()
+                        }}>
+                    {test.test.name}
+                        <button className="pkg-info-btn" onClick={() => { toggle('showPackageInfo', test) }}>
+                                <img src={ASSETS_BASE_URL + "/img/customer-icons/info.svg"} />
+                            </button>
+                        
+                    </p>
+                    <input type="checkbox" value="on" checked={this.props.test.is_selected ? true : false} onChange={(e) => {
+                        this.props.toggleTest.bind(this, test)
+                        e.preventDefault()
+                        e.stopPropagation()
+                    }}/>
+                    <span className="checkmark"></span>
+
                 </label>
+                <button className="pkg-info-btn info-san" onClick={() => this.packageNameClick()}>
+                        <span className="">{this.state.testListVisible?'View less':'View all'}</span>
+                    </button>
                 {
                     test.hide_price ? "" :<span className="test-price text-sm">₹ {deal_price}<span className="test-mrp">₹ {mrp}</span></span>
 
