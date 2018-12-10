c89bc07524d6d58c1212db8ea5cb92da4688b9c9
prince kumar <princek@policybazaar.com>
2018-12-10 20:13:48 +0530

    scroll reset

--- dev/js/components/opd/patientDetails/choosePatientNew.js
+++ dev/js/components/opd/patientDetails/choosePatientNew.js
@@ -16,9 +16,6 @@ class ChoosePatientNewView extends React.Component {
     }
 
     componentDidMount() {
-        if (window) {
-            window.scrollTo(0, 0)
-        }
         if (!this.props.patient) {
             this.profileValidation()
         }
