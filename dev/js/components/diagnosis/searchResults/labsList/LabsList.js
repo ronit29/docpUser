13c070c04a336441951e361ac153bbccb017300b
Mayank <mayankyadav403@gmail.com>
2018-12-18 13:01:05 +0530

    lab card

--- dev/js/components/diagnosis/searchResults/labsList/LabsList.js
+++ dev/js/components/diagnosis/searchResults/labsList/LabsList.js
@@ -99,6 +99,94 @@ class LabsList extends React.Component {
                     this.state.renderBlock ? <Loader /> :
                         <div className="container-fluid">
                             <div className="row">
+
+
+                                {/* ====================== start lab view more card ====================== */}
+
+                                <div className="col-12">
+                                    <div className="filter-card-dl mb-3">
+                                        <div className="fltr-crd-top-container">
+                                            <div className="fltr-lctn-dtls">
+                                                <p><img className="fltr-loc-ico" src="/assets/img/customer-icons/map-marker-blue.svg" style={{ width: '12px', height: '18px' }} /><span className="fltr-loc-txt">Gurgaon, Gurgaon</span><span>&nbsp;|&nbsp;3 Km</span></p>
+                                            </div>
+                                            <div className="row no-gutters mrt-10">
+                                                <div className="col-12">
+                                                    <a href="/dr-gaurav-gupta-dentist-implantologist-general-physician-in-sector-11-gurgaon-dpp">
+                                                        <h2 className="lab-fltr-dc-name fw-500 text-md">Metropolis Healthcare Ltd - Gurgaon</h2>
+                                                    </a>
+                                                    <span className="filtr-offer ofr-ribbon fw-700">41% OFF</span>
+                                                </div>
+                                                <div className="col-7 mrt-10">
+                                                    <div className="img-nd-dtls">
+                                                        <div className="text-center">
+                                                            <div><img className="fltr-usr-image-lab" src="https://cdn.docprime.com/media/lab/images/90x60/b0dad6f1354821d9af4c5143cc2aeeaa_N5id4Pi.jpg" /></div>
+                                                        </div>
+                                                        <div style={{ marginLeft: '8px' }}></div>
+                                                    </div>
+                                                </div>
+                                                <div className="col-5 mrt-10 text-right" style={{ paddingleft: '8px' }}>
+                                                    <p className="text-primary fw-500 text-lg mrb-10">₹ 559<span className="fltr-cut-price" style={{ verticalAlign: '1px' }}>₹ 950</span></p>
+                                                    <div className="signup-off-container"><span className="signup-off-doc" style={{ fontSize: '12px' }}>+ ₹ 100 OFF <b>on Signup</b> </span></div>
+                                                    <button className="fltr-bkng-btn" style={{ width: '100%' }}>Book Now</button>
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <ul className="fltr-labs-test-selected mrt-10">
+                                                    <span className="fltr-prv-selected-test">Tests Selected</span>
+                                                    <li className="fltr-slected-test">
+                                                        <label style={{ fontWeight: '400' }}>Dengue NS1 antigen detection</label>
+                                                        <p style={{ fontWeight: '400' }}>₹ 360 <span>₹ 600</span></p>
+                                                    </li>
+                                                    <li className="fltr-slected-test">
+                                                        <label style={{ fontWeight: '400' }}>CBC Haemogram</label>
+                                                        <p style={{ fontWeight: '400' }}>₹ 149 <span>₹ 350</span></p>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div className="filtr-card-footer" style={{ background: 'white' }}>
+                                            <div style={{ paddingRight: "8px" }}>
+                                                <p style={{ marginLeft: '0px;' }}>* Inclusive of home visit charges</p>
+                                            </div>
+                                            <div className="text-right" style={{ marginLeft: 'auto;' }}>
+                                                <img src="/assets/img/customer-icons/clock-black.svg" />
+                                                <p style={{ fontSize: '12px' }}>8:00 AM - 6:00 PM</p>
+                                            </div>
+                                        </div>
+                                        <div className="showBookTestListContainer">
+                                            <div className="showBookTestList">
+                                                <ul>
+                                                    <li>
+                                                        <p className="showBookTestListImg"> <img src="/assets/img/new-loc-ico.svg" style={{ marginRight: '8px', width: "12px" }} />Sector 56 Near Metro Gurgaon | 3.6 km </p>
+                                                        <button className="showBookTestListBtn">Book Now</button>
+                                                    </li>
+                                                    <li>
+                                                        <p className="showBookTestListImg"> <img src="/assets/img/new-loc-ico.svg" style={{ marginRight: '8px', width: "12px" }} />Sector 56 Near Metro Gurgaon | 3.6 km </p>
+                                                        <button className="showBookTestListBtn">Book Now</button>
+                                                    </li>
+                                                    <li>
+                                                        <p className="showBookTestListImg"> <img src="/assets/img/new-loc-ico.svg" style={{ marginRight: '8px', width: "12px" }} />Sector 56 Near Metro Gurgaon | 3.6 km </p>
+                                                        <button className="showBookTestListBtn">Book Now</button>
+                                                    </li>
+                                                    <li>
+                                                        <p className="showBookTestListImg"> <img src="/assets/img/new-loc-ico.svg" style={{ marginRight: '8px', width: "12px" }} />Sector 56 Near Metro Gurgaon | 3.6 km </p>
+                                                        <button className="showBookTestListBtn">Book Now</button>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                            <div className="filtr-card-footer">
+                                                <div style={{ paddingRight: "8px" }}>
+                                                    <p style={{ marginLeft: '0px;' }}>View 4 more locations</p>
+                                                </div>
+                                                <div className="text-right" style={{ marginLeft: 'auto;' }}>
+                                                    <img class="" src="/assets/img/customer-icons/dropdown-arrow.svg" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                {/* ====================== end lab view more card ====================== */}
                                 {Object.entries(this.props.selectedCriterias).map(function ([key, value]) {
                                     if (value.show_details) {
                                         show_details = true
