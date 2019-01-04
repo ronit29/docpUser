8b1a7e6f464531a063589b6ddb64d5f6e650ef25
Kunal Batra <>
2019-01-04 12:35:51 +0530

    offer events

--- dev/js/components/commons/offers/OffersView.js
+++ dev/js/components/commons/offers/OffersView.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ProfileHeader from '../DesktopProfileHeader';
 import RightBar from '../RightBar';
 import Footer from '../Home/footer'
+import GTM from '../../../helpers/gtm';
 
 class OffersView extends React.Component {
 
@@ -12,11 +13,22 @@ class OffersView extends React.Component {
             let filters = { 'priceRange': [offer.url_details.min_fees, offer.url_details.max_fees], 'distanceRange': [offer.url_details.min_distance, offer.url_details.max_distance], 'sort_on': offer.url_details.sort_on || '', 'lab_name': offer.url_details.lab_name || '', 'network_id': offer.url_details.network_id || '' }
 
             test.type = 'test'
-            test.id = parseInt(offer.url_details.test_ids)
+            test.id = []
+
+            let testIdArray = offer.url_details.test_ids.split(',');
+            for (let id in testIdArray) {
+                test.id.push(parseInt(testIdArray[id]))
+            }
+
             this.props.toggleDiagnosisCriteria('test', test, true, filters)
             setTimeout(() => {
                 this.props.history.push('/lab/searchresults')
             }, 100)
+
+            let data = {
+                'Category': 'ConsumerApp', 'Action': offer.event_name, 'CustomerID': GTM.getUserId() || '', 'leadid': 0, 'event': offer.event_name, 'clickedOn': offer.slider_location
+            }
+            GTM.sendEvent({ data: data })
         }
 
         else if (offer.url_details && offer.url_details.specializations && offer.url_details.specializations != '') {
@@ -25,11 +37,22 @@ class OffersView extends React.Component {
             let filters = { 'priceRange': [offer.url_details.min_fees, offer.url_details.max_fees], 'distanceRange': [offer.url_details.min_distance, offer.url_details.max_distance], 'sort_on': offer.url_details.sort_on || '', 'is_female': offer.url_details.is_female || false, 'is_available': offer.url_details.is_available || false, 'doctor_name': offer.url_details.doctor_name || '', 'hospital_name': offer.url_details.hospital_name || '', 'hospital_id': offer.url_details.hospital_id || '' }
 
             speciality.type = 'speciality'
-            speciality.id = parseInt(offer.url_details.specializations)
+            speciality.id = []
+
+            let specialityIdArray = offer.url_details.specializations.split(',');
+            for (let id in specialityIdArray) {
+                speciality.id.push(parseInt(specialityIdArray[id]))
+            }
+
             this.props.toggleOPDCriteria('speciality', speciality, true, filters)
             setTimeout(() => {
                 this.props.history.push('/opd/searchresults')
             }, 100)
+
+            let data = {
+                'Category': 'ConsumerApp', 'Action': offer.event_name, 'CustomerID': GTM.getUserId() || '', 'leadid': 0, 'event': offer.event_name, 'clickedOn': offer.slider_location
+            }
+            GTM.sendEvent({ data: data })
         }
 
         else if (offer.url_details && offer.url_details.procedure_ids && offer.url_details.procedure_ids != '') {
@@ -38,11 +61,22 @@ class OffersView extends React.Component {
             let filters = { 'priceRange': [offer.url_details.min_fees, offer.url_details.max_fees], 'distanceRange': [offer.url_details.min_distance, offer.url_details.max_distance], 'sort_on': offer.url_details.sort_on || '', 'is_female': offer.url_details.is_female || false, 'is_available': offer.url_details.is_available || false, 'doctor_name': offer.url_details.doctor_name || '', 'hospital_name': offer.url_details.hospital_name || '', 'hospital_id': offer.url_details.hospital_id || '' }
 
             speciality.type = 'procedures'
-            speciality.id = parseInt(offer.url_details.procedure_ids)
+            speciality.id = []
+
+            let specialityIdArray = offer.url_details.procedure_ids.split(',');
+            for (let id in specialityIdArray) {
+                speciality.id.push(parseInt(specialityIdArray[id]))
+            }
+
             this.props.toggleOPDCriteria('procedures', speciality, true, filters)
             setTimeout(() => {
                 this.props.history.push('/opd/searchresults')
             }, 100)
+
+            let data = {
+                'Category': 'ConsumerApp', 'Action': offer.event_name, 'CustomerID': GTM.getUserId() || '', 'leadid': 0, 'event': offer.event_name, 'clickedOn': offer.slider_location
+            }
+            GTM.sendEvent({ data: data })
         }
 
         else if (offer.url_details && offer.url_details.procedure_category_ids && offer.url_details.procedure_category_ids != '') {
@@ -51,11 +85,22 @@ class OffersView extends React.Component {
             let filters = { 'priceRange': [offer.url_details.min_fees, offer.url_details.max_fees], 'distanceRange': [offer.url_details.min_distance, offer.url_details.max_distance], 'sort_on': offer.url_details.sort_on || '', 'is_female': offer.url_details.is_female || false, 'is_available': offer.url_details.is_available || false, 'doctor_name': offer.url_details.doctor_name || '', 'hospital_name': offer.url_details.hospital_name || '', 'hospital_id': offer.url_details.hospital_id || '' }
 
             speciality.type = 'procedures_category'
-            speciality.id = parseInt(offer.url_details.procedure_category_ids)
+            speciality.id = []
+
+            let specialityIdArray = offer.url_details.procedure_category_ids.split(',');
+            for (let id in specialityIdArray) {
+                speciality.id.push(parseInt(specialityIdArray[id]))
+            }
+
             this.props.toggleOPDCriteria('procedures_category', speciality, true, filters)
             setTimeout(() => {
                 this.props.history.push('/opd/searchresults')
             }, 100)
+
+            let data = {
+                'Category': 'ConsumerApp', 'Action': offer.event_name, 'CustomerID': GTM.getUserId() || '', 'leadid': 0, 'event': offer.event_name, 'clickedOn': offer.slider_location
+            }
+            GTM.sendEvent({ data: data })
         }
 
         else if (offer.url_details && offer.url_details.conditions && offer.url_details.conditions != '') {
@@ -64,43 +109,32 @@ class OffersView extends React.Component {
             let filters = { 'priceRange': [offer.url_details.min_fees, offer.url_details.max_fees], 'distanceRange': [offer.url_details.min_distance, offer.url_details.max_distance], 'sort_on': offer.url_details.sort_on || '', 'is_female': offer.url_details.is_female || false, 'is_available': offer.url_details.is_available || false, 'doctor_name': offer.url_details.doctor_name || '', 'hospital_name': offer.url_details.hospital_name || '', 'hospital_id': offer.url_details.hospital_id || '' }
 
             speciality.type = 'condition'
-            speciality.id = parseInt(offer.url_details.conditions)
+            speciality.id = []
+
+            let specialityIdArray = offer.url_details.conditions.split(',');
+            for (let id in specialityIdArray) {
+                speciality.id.push(parseInt(specialityIdArray[id]))
+            }
+
             this.props.toggleOPDCriteria('condition', speciality, true, filters)
             setTimeout(() => {
                 this.props.history.push('/opd/searchresults')
             }, 100)
+
+            let data = {
+                'Category': 'ConsumerApp', 'Action': offer.event_name, 'CustomerID': GTM.getUserId() || '', 'leadid': 0, 'event': offer.event_name, 'clickedOn': offer.slider_location
+            }
+            GTM.sendEvent({ data: data })
         }
 
         else if (offer.url) {
             this.props.history.push(offer.url)
-        }
 
-        // if (id === 1) {
-        //     let test = {}
-        //     test.type = 'test'
-        //     test.id = 12227
-        //     this.props.toggleDiagnosisCriteria('test', test, true)
-        //     setTimeout(() => {
-        //         this.props.history.push('/lab/searchresults')
-        //     }, 100)
-        // } else if (id === 2) {
-        //     let test = {}
-        //     test.type = 'test'
-        //     test.id = 11554
-        //     this.props.toggleDiagnosisCriteria('test', test, true)
-        //     setTimeout(() => {
-        //         this.props.history.push('/lab/searchresults')
-        //     }, 100)
-        // } else if (id === 3) {
-        //     let speciality = {}
-        //     speciality.type = 'procedures_category'
-        //     speciality.id = 2
-        //     let filters = { 'sort_on': 'fees' }
-        //     this.props.toggleOPDCriteria('procedures_category', speciality, true, filters)
-        //     setTimeout(() => {
-        //         this.props.history.push('/opd/searchresults')
-        //     }, 100)
-        // }
+            let data = {
+                'Category': 'ConsumerApp', 'Action': offer.event_name, 'CustomerID': GTM.getUserId() || '', 'leadid': 0, 'event': offer.event_name, 'clickedOn': offer.slider_location
+            }
+            GTM.sendEvent({ data: data })
+        }
     }
 
     componentDidMount() {
@@ -108,8 +142,6 @@ class OffersView extends React.Component {
     }
 
     render() {
-        console.log('gsgusdg')
-        console.log(this.props.offerList)
         return (
             <div className="profile-body-wrap" style={{ background: '#fff' }}>
                 <ProfileHeader showSearch={true} />
@@ -122,8 +154,6 @@ class OffersView extends React.Component {
                                         <p className="fw-700 offer-heading mrt-20">Offers</p>
                                     </div>
                                     <div className="col-12">
-
-
                                         {
                                             this.props.offerList ?
                                                 this.props.offerList.map((offer, i) => {
@@ -134,20 +164,6 @@ class OffersView extends React.Component {
                                                     }
                                                 }) : ''
                                         }
-
-
-                                        {/* <div className="offer-div">
-                                            <img src={ASSETS_BASE_URL + '/img/banners/banner_paytm.png'} style={{ cursor: 'unset' }} />
-                                        </div>
-                                        <div className="offer-div" onClick={() => this.navigateTo(1)}>
-                                            <img src={ASSETS_BASE_URL + '/img/banners/banner_aarogyam_new.png'} />
-                                        </div>
-                                        <div className="offer-div" onClick={() => this.navigateTo(2)}>
-                                            <img src={ASSETS_BASE_URL + '/img/banners/banner_ultrasound.png'} />
-                                        </div>
-                                        <div className="offer-div" onClick={() => this.navigateTo(3)}>
-                                            <img src={ASSETS_BASE_URL + '/img/banners/banner_teeth.png'} />
-                                        </div> */}
                                     </div>
                                 </div>
                             </div>
