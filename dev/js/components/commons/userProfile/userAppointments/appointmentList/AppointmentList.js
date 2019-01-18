cf68ffeb5550905dbd0fd5fee12e913680f087df
Kunal Batra <>
2019-01-18 15:59:08 +0530

    added download invoice option on my appointment page

--- dev/js/components/commons/userProfile/userAppointments/appointmentList/AppointmentList.js
+++ dev/js/components/commons/userProfile/userAppointments/appointmentList/AppointmentList.js
@@ -62,29 +62,43 @@ class AppointmentList extends React.Component {
         }
     }
 
+    invoiceClick(invoiceLink) {
+        var win = window.open(invoiceLink, '_blank');
+        win.focus();
+    }
+
     render() {
 
-        let { doctor_name, display_name, time_slot_end, time_slot_start, status, type, id, lab_name, doctor_thumbnail, lab_thumbnail, patient_name } = this.props.data
+        let { doctor_name, display_name, time_slot_end, time_slot_start, status, type, id, lab_name, doctor_thumbnail, lab_thumbnail, patient_name, invoices } = this.props.data
 
         let date = new Date(time_slot_start)
 
         return (
-            <li onClick={this.openAppointment.bind(this, type, id)} style={{ position: 'relative', paddingTop: 25 }}>
-                <a>
-                    <span className="icon consultant-dp">
-                        <InitialsPicture name={(doctor_name || lab_name)} has_image={!!(doctor_thumbnail || lab_thumbnail)} className="initialsPicture-appointment">
-                            <img src={doctor_thumbnail || lab_thumbnail} className="img-fluid img-round my-appont-img" />
-                        </InitialsPicture>
+            <li style={{ position: 'relative', paddingTop: 25, cursor: 'unset' }}>
+                <span className="icon consultant-dp">
+                    <InitialsPicture name={(doctor_name || lab_name)} has_image={!!(doctor_thumbnail || lab_thumbnail)} className="initialsPicture-appointment" style={{ position: 'relative' }}>
+                        <img src={doctor_thumbnail || lab_thumbnail} className="img-fluid img-round my-appont-img" />
                         {type == 'doctor' ? <img src={ASSETS_BASE_URL + "/img/customer-icons/stethoscope.svg"} className="appointment-icon" /> : <img src={ASSETS_BASE_URL + "/img/customer-icons/beaker.svg"} className="appointment-icon" />}
-                    </span>
-                    <div className="consultant-details">
-                        <h4 className="title app-title" style={{marginBottom: 8}} >{display_name || lab_name}</h4>
-                        <ul className="list">
-                            <li style={{marginBottom: 5}} ><span className="ct-img ct-img-xs"><img src={ASSETS_BASE_URL + "/img/icons/calendar.svg"} className="img-fluid" /></span>{date.toDateString()} </li>
-                            <li style={{marginBottom: 5}} ><span className="ct-img ct-img-xs"><img src={ASSETS_BASE_URL + "/img/customer-icons/clock.svg"} className="img-fluid" /></span>{this.getTime(time_slot_start)}</li>
-                            <li style={{marginBottom: 5}} ><span className="ct-img ct-img-xs"><img src={ASSETS_BASE_URL + "/img/icons/user.svg"} className="img-fluid" style={{ width: 14, marginTop: -4 }} /></span>{patient_name}</li>
-                        </ul>
-                        {/* <div className="view-chat text-right">
+                    </InitialsPicture>
+                    {
+                        invoices && invoices.length === 1 ?
+                            <div className="mrt-10 invoice-div" onClick={() => this.invoiceClick(invoices[0])}>
+                                <img src={ASSETS_BASE_URL + '/img/customer-icons/invoice.svg'} />
+                                <div>
+                                    <p className="text-primary fw-500">Download</p>
+                                    <p className="text-primary fw-500">Invoice</p>
+                                </div>
+                            </div> : ''
+                    }
+                </span>
+                <div className="consultant-details" style={{ cursor: 'pointer' }} onClick={this.openAppointment.bind(this, type, id)}>
+                    <h4 className="title app-title" style={{ marginBottom: 8 }} >{display_name || lab_name}</h4>
+                    <ul className="list">
+                        <li style={{ marginBottom: 5 }} ><span className="ct-img ct-img-xs"><img src={ASSETS_BASE_URL + "/img/icons/calendar.svg"} className="img-fluid" /></span>{date.toDateString()} </li>
+                        <li style={{ marginBottom: 5 }} ><span className="ct-img ct-img-xs"><img src={ASSETS_BASE_URL + "/img/customer-icons/clock.svg"} className="img-fluid" /></span>{this.getTime(time_slot_start)}</li>
+                        <li style={{ marginBottom: 5 }} ><span className="ct-img ct-img-xs"><img src={ASSETS_BASE_URL + "/img/icons/user.svg"} className="img-fluid" style={{ width: 14, marginTop: -4 }} /></span>{patient_name}</li>
+                    </ul>
+                    {/* <div className="view-chat text-right">
                             {
                                 (status == 7 && type == 'doctor') ? <button onClick={this.viewReports.bind(this, type, id)} className="v-btn v-btn-primary">View Prescription</button> : ""
                             }
@@ -93,14 +107,30 @@ class AppointmentList extends React.Component {
                                 (status == 7 && type == 'lab') ? <button onClick={this.viewReports.bind(this, type, id)} className="v-btn v-btn-primary">View Report</button> : ""
                             }
                         </div> */}
+                </div>
+                <span className="arrow-custom-right" style={{ cursor: 'pointer' }} onClick={this.openAppointment.bind(this, type, id)}><img src={ASSETS_BASE_URL + "/img/customer-icons/arrow-forward-right.svg"} /></span>
+                {this.getStatus(status)}
+                {/* <div className="mrt-20 multiple-invoice-div">
+                    <div className="multiple-invoice">
+                        <div className="invoice-div">
+                            <img src={ASSETS_BASE_URL + '/img/customer-icons/invoice.svg'} />
+                            <div>
+                                <p className="text-primary fw-500">Download</p>
+                                <p className="text-primary fw-500">Invoice</p>
+                            </div>
+                        </div>
+                        <div className="invoice-div">
+                            <img src={ASSETS_BASE_URL + '/img/customer-icons/report.svg'} />
+                            <div>
+                                <p className="text-primary fw-500">View</p>
+                                <p className="text-primary fw-500">Report</p>
+                            </div>
+                        </div>
                     </div>
-                    <span className="arrow-custom-right"><img src={ASSETS_BASE_URL + "/img/customer-icons/arrow-forward-right.svg"} /></span>
-                    {this.getStatus(status)}
-                </a>
+                </div> */}
             </li>
         );
     }
 }
-
 
 export default AppointmentList
