071117771486bc88139ca46dbcad7547e8c01cf9
Kunal Batra <>
2019-01-18 17:30:57 +0530

    invoice and report added

--- dev/js/components/commons/userProfile/userAppointments/appointmentList/AppointmentList.js
+++ dev/js/components/commons/userProfile/userAppointments/appointmentList/AppointmentList.js
@@ -69,7 +69,7 @@ class AppointmentList extends React.Component {
 
     render() {
 
-        let { doctor_name, display_name, time_slot_end, time_slot_start, status, type, id, lab_name, doctor_thumbnail, lab_thumbnail, patient_name, invoices } = this.props.data
+        let { doctor_name, display_name, time_slot_end, time_slot_start, status, type, id, lab_name, doctor_thumbnail, lab_thumbnail, patient_name, invoices, reports } = this.props.data
 
         let date = new Date(time_slot_start)
 
@@ -81,7 +81,7 @@ class AppointmentList extends React.Component {
                         {type == 'doctor' ? <img src={ASSETS_BASE_URL + "/img/customer-icons/stethoscope.svg"} className="appointment-icon" /> : <img src={ASSETS_BASE_URL + "/img/customer-icons/beaker.svg"} className="appointment-icon" />}
                     </InitialsPicture>
                     {
-                        invoices && invoices.length === 1 ?
+                        invoices && invoices.length === 1 && !reports.length ?
                             <div className="mrt-10 invoice-div" onClick={() => this.invoiceClick(invoices[0])}>
                                 <img src={ASSETS_BASE_URL + '/img/customer-icons/invoice.svg'} />
                                 <div>
@@ -110,24 +110,29 @@ class AppointmentList extends React.Component {
                 </div>
                 <span className="arrow-custom-right" style={{ cursor: 'pointer' }} onClick={this.openAppointment.bind(this, type, id)}><img src={ASSETS_BASE_URL + "/img/customer-icons/arrow-forward-right.svg"} /></span>
                 {this.getStatus(status)}
-                {/* <div className="mrt-20 multiple-invoice-div">
-                    <div className="multiple-invoice">
-                        <div className="invoice-div">
-                            <img src={ASSETS_BASE_URL + '/img/customer-icons/invoice.svg'} />
-                            <div>
-                                <p className="text-primary fw-500">Download</p>
-                                <p className="text-primary fw-500">Invoice</p>
+                {
+                    invoices && invoices.length && reports.length ?
+                        <div className="mrt-20 multiple-invoice-div">
+                            <div className="multiple-invoice">
+                                <div className="invoice-div" onClick={() => this.invoiceClick(invoices[0])}>
+                                    <img src={ASSETS_BASE_URL + '/img/customer-icons/invoice.svg'} />
+                                    <div>
+                                        <p className="text-primary fw-500">Download</p>
+                                        <p className="text-primary fw-500">Invoice</p>
+                                    </div>
+                                </div>
+                                <div className="invoice-div" onClick={() => {
+                                    this.props.history.push(`/user/lab/reports/${id}`)
+                                }}>
+                                    <img src={ASSETS_BASE_URL + '/img/customer-icons/report.svg'} />
+                                    <div>
+                                        <p className="text-primary fw-500">View</p>
+                                        <p className="text-primary fw-500">Report</p>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div className="invoice-div">
-                            <img src={ASSETS_BASE_URL + '/img/customer-icons/report.svg'} />
-                            <div>
-                                <p className="text-primary fw-500">View</p>
-                                <p className="text-primary fw-500">Report</p>
-                            </div>
-                        </div>
-                    </div>
-                </div> */}
+                        </div> : ''
+                }
             </li>
         );
     }
