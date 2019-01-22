5265e5629d6538af2dac286e029d4aeeb8c1052b
rj_rishab28 <rishabsurana@docprime.com>
2019-01-22 11:37:52 +0530

    Done testinfo in case of package test list

--- dev/js/components/diagnosis/commons/labTests/labTests.js
+++ dev/js/components/diagnosis/commons/labTests/labTests.js
@@ -73,7 +73,7 @@ class LabTests extends React.Component {
 
                 if (test.is_package) {
                     if (test.is_selected) {
-                        selectedPackage.push(<PackageTest key={i} i={i} test={test} toggle={this.toggle.bind(this)} toggleTest={this.toggleTest.bind(this)} />)
+                        selectedPackage.push(<PackageTest key={i} i={i} test={test} toggle={this.toggle.bind(this)} toggleTest={this.toggleTest.bind(this)} testInfo={this.testInfo.bind(this)}/>)
                     } else {
                         unSelectedPackage.push(<PackageTest key={i} i={i} test={test} toggle={this.toggle.bind(this)} toggleTest={this.toggleTest.bind(this)} />)
                     }
