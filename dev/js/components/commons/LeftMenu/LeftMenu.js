b30acc17978a97d6fd69b0e794684824daf80bf1
Kunal Kumar <kunalkumar@docprime.com>
2019-01-29 11:12:32 +0530

    left-menu added

---
+++ dev/js/components/commons/LeftMenu/LeftMenu.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+class LeftMenu extends React.Component {
+  render(){
+    return(
+           
+            <section>
+                <div className="container">
+                  <div className="row">
+                     <div className="col-xs-12 col-d-width">
+                        <div className="left-menu">
+                            <div className="header-box">
+                              <img src="images/profile-photo.jpeg" alt="profile-photo" className="user-profile-img" />
+                              <span className="user-name">Rishabh Malohtra</span>
+                              <span className="right-arrow r-arrow"></span>
+                            </div>
+                            <ul className="drop-list-menu list_1">
+                              <li><a href="#"><img src="images/insurance.png" alt="" className="" />Insurance</a> <a href="#" class="btn-buy-now">Buy Now</a></li>
+                              <li><a href="#"><img src="images/online-prescription.png" alt="" className="" />My Online Prescription</a></li>
+                              <li><a href="#"><img src="images/my-appointment.png" alt="" className="" />My Appoinments</a></li>
+                              <li>
+                                  <a href="#"><img src="images/my-wallet.png" alt="" className="" />My Wallet</a>
+                                  <span className="wallet-amnt"><img src="images/rupees-icon.png" />212</span>
+                              </li>
+                              <li><a href="#"><img src="images/refer-and-earn.png" alt="" className="" />Refer and Earn</a></li>
+                              <li><a href="#"><img src="images/notification.png" alt="" className="" />Notification</a></li>
+                            </ul>
+                            
+                            <ul className="drop-list-menu list_2">
+                              <li><a href="#" className="pad-B0"><img src="images/myprofile.png" alt="" className="" />My Profile</a></li>
+                              <li><a href="#" className="pad-B0">My Family</a></li>
+                              <li><a href="#">My Address</a></li>
+                            </ul>
+                            
+                            <ul className="drop-list-menu">
+                              <li><a href="#"><img src="images/articles.png" alt="" className="" />Articles</a></li>
+                              <li><a href="#"><img src="images/rate-us.png" alt="" className="" />Rate us</a></li>
+                            </ul>
+                            
+                            <ul className="final-list">
+                              <li><a href="#">About Us</a></li>
+                              <li><a href="#">Terms and Condition</a></li>
+                              <li className="logout"><a href="#">Logout</a></li>
+                            </ul>
+
+                        </div>
+                    </div>
+                  </div>
+              </div>
+            </section>
+  
+      )
+  }
+}
+export default LeftMenu
