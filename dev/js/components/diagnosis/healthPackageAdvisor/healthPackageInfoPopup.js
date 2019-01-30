87991c5ffa2a5c006bef1d6e813d5118093954b6
rj_rishab28 <rishabsurana@docprime.com>
2019-01-30 21:19:16 +0530

    done

--- dev/js/components/diagnosis/healthPackageAdvisor/healthPackageInfoPopup.js
+++ dev/js/components/diagnosis/healthPackageAdvisor/healthPackageInfoPopup.js
@@ -12,7 +12,7 @@ class InfoPopup extends React.Component {
                 <div className="cancel-overlay" onClick={() => {this.props.closeInfo()}}></div>
                 <div className="widget cancel-appointment-div payment-popup">
                     <div className="widget-header text-center">
-                        <p className="fw-500 cancel-appointment-head">{this.props.infoTextId == 1?this.props.package_information.physical:this.props.package_information.screening}</p>
+                        <p className="fw-500 cancel-appointment-head">{this.props.infoTextId == 1?this.props.package_information.screening:this.props.package_information.physical}</p>
                     </div>
                 </div>
             </div>
