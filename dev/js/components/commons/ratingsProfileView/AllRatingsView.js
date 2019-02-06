261b9906f25b4fe9599ac26e5846f7ce4a226b9a
ronit <ronitd@policybazaar.com>
2019-02-06 16:29:56 +0530

    page update

--- dev/js/components/commons/ratingsProfileView/AllRatingsView.js
+++ dev/js/components/commons/ratingsProfileView/AllRatingsView.js
@@ -1,15 +1,20 @@
 import React from "react";
 const queryString = require('query-string');
 
+import ProfileHeader from '../../commons/DesktopProfileHeader'
 import ComplimentListView from '../../commons/ratingsProfileView/ComplimentListView.js'
 import ReviewList from '../../commons/ratingsProfileView/ReviewList.js'
 import RatingGraph from '../../commons/ratingsProfileView/RatingGraph.js'
+import LeftBar from '../../commons/LeftBar'
+import RightBar from '../../commons/RightBar'
+import Footer from '../../commons/Home/footer'
 
 class AllRatingsView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             data: null,
+            footerData: null,
         }
     }
 
@@ -25,21 +30,37 @@ class AllRatingsView extends React.Component {
     }
 
     render() {
-        if (this.state.data) {
-            return (
-                <div>
-                    <RatingGraph details={this.state.data} />
-                    <div className="user-satisfaction-section">
-                        <div className="row no-gutters">
-                            {this.state.data ? this.state.data.rating_graph.top_compliments.map(compliment =>
-                                <ComplimentListView key={compliment.id} details={compliment} />) : ""}
+        return (
+            <div className="profile-body-wrap">
+                <ProfileHeader showSearch={true} />
+                <section className="container parent-section book-appointment-section breadcrumb-mrgn">
+                    <div className="row main-row parent-section-row">
+                        <LeftBar />
+                        <div className="col-12 col-md-7 col-lg-7 center-column">
+                            <div className="container-fluid">
+                                <div className="row">
+                                    <div className="col-12">
+                                        {this.state.data ?
+                                            <div>
+                                                <RatingGraph details={this.state.data} />
+                                                <div className="user-satisfaction-section">
+                                                    <div className="row no-gutters">
+                                                        {this.state.data ? this.state.data.rating_graph.top_compliments.map(compliment =>
+                                                            <ComplimentListView key={compliment.id} details={compliment} />) : ""}
+                                                    </div>
+                                                </div>
+                                                <ReviewList details={this.state.data} />
+                                            </div> : ""}
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+                        <RightBar />
                     </div>
-                    <ReviewList details={this.state.data} />
-                </div>)
-        } else {
-            return <div></div>;
-        }
+                </section>
+                <Footer footerData={this.state.footerData} />
+            </div>
+        )
     }
 
 }
