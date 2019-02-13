fdd543f6daab66c36d5b2dd116f4080fd320e0b5
rj_rishab28 <rishabsurana@docprime.com>
2019-02-13 15:53:35 +0530

    url flag changed

--- dev/js/routes.js
+++ dev/js/routes.js
@@ -132,7 +132,7 @@ const routes = [
     { path: '/full-body-checkup-health-packages', exact: true, component: DX_SearchPackages, RENDER_ON_SERVER: true },
     { path: '/health-package-advisor', exact: true, component: HealthPackageAdvisor, RENDER_ON_SERVER: true },
     { path: '/searchpackages', exact: true, component: DX_SearchPackages, redirect: false, redirectTo: "full-body-checkup-health-packages", RENDER_ON_SERVER: true },
-    { path: `/*-ltpp`, component: searchTestInfo, RENDER_ON_SERVER: true },
+    { path: `/*-tpp`, component: searchTestInfo, RENDER_ON_SERVER: true },
     { path: '*', component: NotFound, NO_MATCH: true }
 ]
 
