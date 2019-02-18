f1ea08c7b607483391cb5bd53c6e5d065be5d7d5
rj_rishab28 <rishabsurana@docprime.com>
2019-02-18 14:12:27 +0530

    D

--- dev/js/components/diagnosis/healthPackageAdvisor/HealthPackageAdvisorView.js
+++ dev/js/components/diagnosis/healthPackageAdvisor/HealthPackageAdvisorView.js
@@ -75,48 +75,114 @@ class HealthPackageAdvisorView extends React.Component {
         }
         self.setState({ testInfoIds: test_ids })
     }
+    // selectCategory(cat_id, isSubset,cat_data) {
+    //     let ids = this.state.selectCatIDs.filter(x => parseInt(x.cat_id) == parseInt(cat_id))
+    //     let selected_catIds = [].concat(this.state.selectCatIDs)
+    //     let selected_testids= [].concat(this.state.selectedTestIds)
+    //     let subSetTestIds = []
+    //     let initialDataVal = []
+    //     if (ids.length) {
+    //         initialDataVal = this.state.initialData.filter(x => parseInt(x.cat_id) == parseInt(cat_id))
+    //         if (initialDataVal[0].subSetTest.length != ids[0].subSetTest.length ) {
+    //             selected_catIds = this.state.selectCatIDs.filter(x => parseInt(x.cat_id) != parseInt(cat_id))
+    //             Object.entries(cat_data.tests).map(function ([key, value]) {
+    //                 selected_testids.push(value.id)
+    //                 subSetTestIds.push(value.id)
+    //             })
+    //             selected_catIds.push({ cat_id: cat_id, isSubset: ids[0].isSubset, subSetTest:subSetTestIds })
+    //         } else {
+    //             if (ids[0].cat_id == cat_id) {
+    //                 Object.entries(ids[0].subSetTest).map(function ([key, value]) {
+    //                     selected_testids = selected_testids.filter(x => parseInt(x) != parseInt(value))
+    //                 })
+    //                 selected_catIds = selected_catIds.filter(x => parseInt(x.cat_id) != parseInt(cat_id))
+    //             }
+    //         }
+    //     } else {
+    //         Object.entries(cat_data.tests).map(function ([key, value]) {
+    //             selected_testids.push(value.id)
+    //             subSetTestIds.push(value.id)
+    //         })
+    //         selected_catIds.push({ cat_id: cat_id, isSubset: isSubset, subSetTest: subSetTestIds })
+    //     }
+    //     selected_testids = selected_testids.filter(function(item, index){
+    //         return selected_testids.indexOf(item) >= index;
+    //     })
+    //     this.setState({ selectCatIDs: selected_catIds, selectedTestIds:selected_testids })
+    // }
+    // selectTest(test_id, package_id) {
+    //     let test_ids = [].concat(this.state.selectedTestIds)
+    //     let self = this
+    //     let found = false
+
+    //     test_ids = test_ids.filter((x) => {
+    //         if (x == test_id) {
+    //             found = true
+    //             return false
+    //         }
+    //         return true
+    //     })
+
+    //     if (!found) {
+    //         test_ids.push(test_id)
+    //     }
+
+    //     let package_ids = []
+    //     let selectedIds = []
+    //     let finalIds = [].concat(this.state.selectCatIDs)
+    //     if (this.state.selectCatIDs.length > 0) {
+    //         selectedIds = this.state.selectCatIDs.filter(x => parseInt(x.cat_id) == parseInt(package_id))
+    //         package_ids = this.state.selectCatIDs.filter(x => parseInt(x.cat_id) != parseInt(package_id
+    //             ))
+    //         if (selectedIds.length > 0) {
+    //             Object.entries(selectedIds).map(function ([key, value]) {
+
+    //                 if (value.cat_id == package_id) {
+    //                     let found = false
+    //                     value.isSubset = false
+    //                     value.subSetTest = value.subSetTest.filter((x) => {
+    //                         if (x == test_id) {
+    //                             found = true
+    //                             return false
+    //                         }
+    //                         return true
+    //                     })
+    //                     if (!found) {
+    //                         value.subSetTest.push(test_id)
+    //                     }
+    //                 }
+    //             })
+    //             finalIds = [...package_ids, ...selectedIds]
+    //         }
+    //         if(package_ids.length > 0){
+    //             Object.entries(package_ids).map(function ([key, val]) {
+    //                 if(selectedIds.length>0){
+    //                     Object.entries(package_ids).map(function ([key, selectedId]) {
+    //                         if(parseInt(val.cat_id) != parseInt(package_id) && selectedId.cat_id != package_id){
+    //                             return
+    //                         } 
+    //                     })
+    //                 }else{
+    //                     if(parseInt(val.cat_id) != parseInt(package_id)){
+    //                         finalIds.push({ cat_id: package_id, isSubset: false, subSetTest: [].concat(test_id) })
+    //                     }
+    //                 }
+    //             })                
+    //             finalIds = Array.from(new Set(finalIds.map(JSON.stringify))).map(JSON.parse)
+    //         }
+    //     }else{
+    //         finalIds.push({ cat_id: package_id, isSubset: false, subSetTest: [].concat(test_id) })
+    //     }
+    //     self.setState({ selectedTestIds: test_ids, selectCatIDs: finalIds})
+    // }
+
     selectCategory(cat_id, isSubset,cat_data) {
-        let ids = this.state.selectCatIDs.filter(x => parseInt(x.cat_id) == parseInt(cat_id))
         let selected_catIds = [].concat(this.state.selectCatIDs)
-        let selected_testids= [].concat(this.state.selectedTestIds)
-        let subSetTestIds = []
-        let initialDataVal = []
-        if (ids.length) {
-            initialDataVal = this.state.initialData.filter(x => parseInt(x.cat_id) == parseInt(cat_id))
-            if (initialDataVal[0].subSetTest.length != ids[0].subSetTest.length ) {
-                selected_catIds = this.state.selectCatIDs.filter(x => parseInt(x.cat_id) != parseInt(cat_id))
-                Object.entries(cat_data.tests).map(function ([key, value]) {
-                    selected_testids.push(value.id)
-                    subSetTestIds.push(value.id)
-                })
-                selected_catIds.push({ cat_id: cat_id, isSubset: ids[0].isSubset, subSetTest:subSetTestIds })
-            } else {
-                if (ids[0].cat_id == cat_id) {
-                    Object.entries(ids[0].subSetTest).map(function ([key, value]) {
-                        selected_testids = selected_testids.filter(x => parseInt(x) != parseInt(value))
-                    })
-                    selected_catIds = selected_catIds.filter(x => parseInt(x.cat_id) != parseInt(cat_id))
-                }
-            }
-        } else {
-            Object.entries(cat_data.tests).map(function ([key, value]) {
-                selected_testids.push(value.id)
-                subSetTestIds.push(value.id)
-            })
-            selected_catIds.push({ cat_id: cat_id, isSubset: isSubset, subSetTest: subSetTestIds })
-        }
-        selected_testids = selected_testids.filter(function(item, index){
-            return selected_testids.indexOf(item) >= index;
-        })
-        this.setState({ selectCatIDs: selected_catIds, selectedTestIds:selected_testids })
-    }
-    selectTest(test_id, package_id) {
-        let test_ids = [].concat(this.state.selectedTestIds)
         let self = this
         let found = false
 
-        test_ids = test_ids.filter((x) => {
-            if (x == test_id) {
+        selected_catIds = selected_catIds.filter((x) => {
+            if (x == cat_id) {
                 found = true
                 return false
             }
@@ -124,56 +190,10 @@ class HealthPackageAdvisorView extends React.Component {
         })
 
         if (!found) {
-            test_ids.push(test_id)
+            selected_catIds.push(cat_id)
         }
 
-        let package_ids = []
-        let selectedIds = []
-        let finalIds = [].concat(this.state.selectCatIDs)
-        if (this.state.selectCatIDs.length > 0) {
-            selectedIds = this.state.selectCatIDs.filter(x => parseInt(x.cat_id) == parseInt(package_id))
-            package_ids = this.state.selectCatIDs.filter(x => parseInt(x.cat_id) != parseInt(package_id
-                ))
-            if (selectedIds.length > 0) {
-                Object.entries(selectedIds).map(function ([key, value]) {
-
-                    if (value.cat_id == package_id) {
-                        let found = false
-                        value.isSubset = false
-                        value.subSetTest = value.subSetTest.filter((x) => {
-                            if (x == test_id) {
-                                found = true
-                                return false
-                            }
-                            return true
-                        })
-                        if (!found) {
-                            value.subSetTest.push(test_id)
-                        }
-                    }
-                })
-                finalIds = [...package_ids, ...selectedIds]
-            }
-            if(package_ids.length > 0){
-                Object.entries(package_ids).map(function ([key, val]) {
-                    if(selectedIds.length>0){
-                        Object.entries(package_ids).map(function ([key, selectedId]) {
-                            if(parseInt(val.cat_id) != parseInt(package_id) && selectedId.cat_id != package_id){
-                                return
-                            } 
-                        })
-                    }else{
-                        if(parseInt(val.cat_id) != parseInt(package_id)){
-                            finalIds.push({ cat_id: package_id, isSubset: false, subSetTest: [].concat(test_id) })
-                        }
-                    }
-                })                
-                finalIds = Array.from(new Set(finalIds.map(JSON.stringify))).map(JSON.parse)
-            }
-        }else{
-            finalIds.push({ cat_id: package_id, isSubset: false, subSetTest: [].concat(test_id) })
-        }
-        self.setState({ selectedTestIds: test_ids, selectCatIDs: finalIds})
+        this.setState({ selectCatIDs: selected_catIds })
     }
     selectAge(event) {
         var event = document.getElementById("selectage")
@@ -220,6 +240,7 @@ class HealthPackageAdvisorView extends React.Component {
         newCategoryState['min_age'] = this.state.min_age
         newCategoryState['gender'] = this.state.gender
         newCategoryState['packageType'] = this.state.packageType
+        console.log(newCategoryState)
         this.props.mergeLABState({ filterCriteriaPackages: newCategoryState })
         setTimeout(() => {
             this.props.history.push('/searchpackages')
@@ -248,7 +269,6 @@ class HealthPackageAdvisorView extends React.Component {
         this.props.history.push('/')
     }
     render() {
-        console.log(this.state.selectCatIDs)
         let self = this
         let selectedCatIDs=[]
         if(this.state.selectCatIDs.length > 0){
@@ -260,7 +280,6 @@ class HealthPackageAdvisorView extends React.Component {
                 })
             })
         }
-        console.log(selectedCatIDs)
         return (
             <div className="profile-body-wrap" style={{ paddingBottom: 54 }} >
                 <ProfileHeader />
@@ -313,9 +332,9 @@ class HealthPackageAdvisorView extends React.Component {
                                                             {this.props.recommended_package.filters && this.props.recommended_package.filters.length > 0 ?
                                                                 Object.entries(this.props.recommended_package.filters).map(function ([key, filter]) {
                                                                     return <div className="dtl-radio d-flex align-items-center dtl-margin-lg pkgTypeBorder" key={key}>
-                                                                        <label className="container-radio mb-0 hpa-container-radio pl-0" style={{ marginRight: 0 }} onChange={self.selectPackage.bind(self, filter.id)}>{filter.name}
-                                                                            {/*<input type="radio" name={`radio2_${filter.id}`} checked={self.state.packageType == filter.id ? true : false} style={{ left: 0 }} />
-                                                                            <span className="doc-checkmark hpa-radio hpa-radio-gender"></span>*/}
+                                                                        <label className="container-radio mb-0 hpa-container-radio" onChange={self.selectPackage.bind(self, filter.id)}>{filter.name}
+                                                                            <input type="radio" name={`radio2_${filter.id}`} checked={self.state.packageType == filter.id ? true : false} style={{ left: 0 }} />
+                                                                            <span className="doc-checkmark hpa-radio hpa-radio-gender"></span>
                                                                         </label>
                                                                         {
                                                                             filter.information != '' ? <img className="hpa-info-icon" src={ASSETS_BASE_URL + "/img/icons/info.svg"} onClick={self.showInfo.bind(self, filter.id)} /> : ''
@@ -365,9 +384,12 @@ class HealthPackageAdvisorView extends React.Component {
                                                     <label className="ck-bx" onChange={self.selectCategory.bind(self, rPackages.id, false,rPackages)}>{rPackages.name}
                                                         {/*<input type="radio" name={`radio_${rPackages.id}`} checked={self.state.selectCatIDs.filter(x => x.cat_id == rPackages.id && !x.isSubset).length ? true : false} />
                                                         <span className="doc-checkmark hpa-radio" style={{ top: 4 }} ></span>*/}
-                                                        <input type="checkbox" value="on" checked={selectedCatIDs.indexOf(rPackages.id) > -1 ? true : false} />
+                                                        <input type="checkbox" value="on" checked={self.state.selectCatIDs.indexOf(rPackages.id) > -1 ? true : false} />
                                                         <span className="checkmark hpa-checkmark"></span>
                                                     </label>
+                                                    <div className="pkgCatImg">
+                                                        <img  className="img-fluid" src="https://cdn.docprime.com/media/diagnostic/common_test_icons/lipid.png"/>
+                                                    </div>
                                                     {/*<label className="ck-bx" style={{ fontSize: 12 }} onChange={self.selectCategory.bind(self, rPackages.id, true)}>Select Test
                                                         {/*<input type="radio" name={`radio_${rPackages.id}`} checked={self.state.selectCatIDs.filter(x => x.cat_id == rPackages.id && x.isSubset).length ? true : false} />
                                                         <span className="doc-checkmark hpa-radio" style={{ top: 0 }}></span>
@@ -375,20 +397,20 @@ class HealthPackageAdvisorView extends React.Component {
                                                         <span className="checkmark hpa-checkmark"></span>
                                                     </label>*/}
                                                 </div>
-                                                <div style={{display: self.state.selectCatIDs.filter(x => x.cat_id == rPackages.id && !x.isSubset).length ?'block':'none'}}>
+                                                {/*<div style={{display: self.state.selectCatIDs.filter(x => x.cat_id == rPackages.id && !x.isSubset).length ?'block':'none'}}>
                                                     <ul className="list hpa-list">
                                                         {
                                                             Object.entries(rPackages.tests).map(function ([k, test]) {
                                                                 return <li key={k}>
                                                                     <div style={{ display: 'block', position: 'relative' }}>
-                                                                        {/*
+                                                                        { //to be comment
                                                                             self.state.selectCatIDs.filter(x => x.cat_id == rPackages.id && x.isSubset).length ?
                                                                                 <label className="ck-bx fw-400" style={{ fontSize: 14, flex: 1, paddingLeft: 24 }} onChange={self.selectTest.bind(self, test.id, rPackages.id)}>{test.name} {test.num_of_parameters != 0 ? '(' + test.num_of_parameters + ')' : ''}
                                                                                     <input type="checkbox" value="on" checked={self.state.selectedTestIds.indexOf(test.id) > -1 ? true : false} />
                                                                                     <span className="checkmark hpa-checkmark"></span>
                                                                                 </label>
                                                                                 : <p className="fw-400" style={{ paddingLeft: 24, lineHeight: '20px' }}>{test.name} {test.num_of_parameters != 0 ? '(' + test.num_of_parameters + ')' : ''}</p>
-                                                                        */}
+                                                                        }
                                                                         <label className="ck-bx fw-400" style={{ fontSize: 14, flex: 1, paddingLeft: 24 }} onChange={self.selectTest.bind(self, test.id, rPackages.id)}>{test.name} {test.num_of_parameters != 0 ? '(' + test.num_of_parameters + ')' : ''}
                                                                             <input type="checkbox" value="on" checked={self.state.selectedTestIds.indexOf(test.id) > -1 ? true : false} />
                                                                             <span className="checkmark hpa-checkmark"></span>
@@ -409,7 +431,7 @@ class HealthPackageAdvisorView extends React.Component {
                                                             })
                                                         }
                                                     </ul>
-                                                </div>
+                                                </div>*/}
                                             </div>
                                         </div>
                                     })}
