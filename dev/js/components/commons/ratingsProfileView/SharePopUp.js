09c05e3c655d762dd27d8fe4af0f3029ec8a675d
ronit <ronitd@policybazaar.com>
2019-02-21 13:20:32 +0530

    updated share

--- dev/js/components/commons/ratingsProfileView/SharePopUp.js
+++ dev/js/components/commons/ratingsProfileView/SharePopUp.js
@@ -64,13 +64,13 @@ class SharePopUp extends React.Component {
             name = profileData.name
         }
         let social_message
-        let doctor_name
+        let url
         if (this.props.details && this.props.details.appointment_type == 2) {
-            doctor_name = this.props.details.entity_name
-            social_message = `Used docprime to book Dr. ${doctor_name}. Had an excellent experience. I love how convenient and easy to use it is. On top of it, you get so many discounts. Who thought healthcare can be so affordable. My first choice for my good health! You can also get ₹ 50 if you signup with my referral code ${this.state.referralCode}.`
+            url = "https://docprime.com/opd/doctor/" + this.props.details.entity_id
         } else {
-            social_message = `Used docprime to book a lab test. Had an excellent experience. I love how convenient and easy to use it is. On top of it, you get so many discounts. Who thought healthcare can be so affordable. My first choice for my good health! You can also get ₹ 50 if you signup with my referral code ${this.state.referralCode}.`
+            url = "https://docprime.com/lab/" + this.props.details.entity_id
         }
+        social_message = `${this.props.details.ratings} out of 5 stars \n ${this.props.details.compliments} \n\n "${this.props.details.review}"`
         return (
             <div className="raiting-popup">
                 <div style={{ backgroundColor: '#fff', borderRadius: 5, position: 'relative' }} >
@@ -78,68 +78,43 @@ class SharePopUp extends React.Component {
                         <img src={ASSETS_BASE_URL + '/img/customer-icons/close-black.svg'} />
                     </div>
                     <div className="home-rating-card" style={{ paddingBottom: 0 }} >
-                        <div className="thankyou-popup-head">
-                            <img src={ASSETS_BASE_URL + "/img/dpsmile.png"} />
-                            <p>Share</p>
-                        </div>
-                        {/* <p className="thnks-content">Your feedback matters!</p> */}
-                        <p className="thnks-content fw-500">REFER &amp; EARN!</p>
-                        <p className="thanks-sub-content mrb-10">Refer your friends & earn ₹ 50</p>
+                        <p className="thnks-content">Your feedback matters!</p>
+                        {/* <p className="thnks-content fw-500">REFER &amp; EARN!</p> */}
+                        <p className="thanks-sub-content mrb-10">Please share with your friends</p>
                         {
                             this.props.selectedRating == 4 || this.props.selectedRating == 5 ?
                                 <div className="social-ico-styling d-flex">
                                     <div className="facebookIcon-styling" onClick={this.gaTracking.bind(this, 'fb')}>
                                         <FacebookShareButton
-                                            url="https://docprime.com"
+                                            url={url}
                                             quote={social_message}
                                             className="button">
                                             <FacebookIcon
                                                 size={32}
                                                 round={false} />
                                         </FacebookShareButton>
-                                        <span>Share referral link</span>
+                                        <span>Facebook</span>
                                     </div>
                                     <div className="twitterIcon-styling" onClick={this.gaTracking.bind(this, 'twitter')}>
                                         <TwitterShareButton
-                                            url="https://docprime.com"
+                                            url={url}
                                             title={social_message}
                                             className="button">
                                             <TwitterIcon
                                                 size={32}
                                                 round={false} />
                                         </TwitterShareButton>
-                                        <span>Tweet referral link</span>
-                                    </div>
-                                    <div className="emailIcon-styling" onClick={this.gaTracking.bind(this, 'email')}>
-                                        <EmailShareButton
-                                            url="https://docprime.com"
-                                            subject="Refer to earn"
-                                            body={`Save upto 50% on doctor appointments and lab tests. Sign up on docprime.com with my code ${this.state.referralCode} and get ₹ 50. Visit ${CONFIG.API_BASE_URL}/login?referral=${this.state.referralCode}`}
-                                            className="button">
-                                            <EmailIcon
-                                                size={32}
-                                                round={false} />
-                                        </EmailShareButton>
-                                        <span>Email referral link</span>
+                                        <span>Twitter</span>
                                     </div>
                                     <div className="whtsappIcon-styling" onClick={this.gaTracking.bind(this, 'whtsapp')} >
                                         <a className="whtsAppico" href={"whatsapp://send?text=" + social_message}>
                                             <img src={ASSETS_BASE_URL + "/img/wa-logo.svg"} />
-                                            <span>Whatsapp referral link</span>
+                                            <span>Whatsapp</span>
                                         </a>
                                     </div>
                                 </div>
                                 : ""
                         }
-                        <CopyToClipboard text={this.getLink()} onCopy={() => { SnackBar.show({ pos: 'bottom-center', text: "Referral Link Copied" }); }} >
-                            <div className="d-flex align-items-center justify-content-center" style={{ cursor: 'pointer' }} >
-                                <img src={ASSETS_BASE_URL + '/img/customer-icons/copy.svg'} style={{ marginRight: 8, width: 20 }} />
-                                <span className="text-primary fw-500">Copy referral link</span>
-                            </div>
-                        </CopyToClipboard>
-                    </div>
-                    <div className="text-center mrt-20" style={{ backgroundColor: 'rgba(247, 134, R, 0.2)', padding: 10, borderBottomLeftRadius: 5, borderBottomRightRadius: 5, cursor: 'pointer' }}>
-                        <a href="/referral" className="fw-500 text-primary">How referral works?</a>
                     </div>
                 </div>
             </div>
