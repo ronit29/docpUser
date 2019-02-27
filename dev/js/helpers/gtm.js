1ccf29eab6160c8ca874dda0461265976ff79b54
Shubham <shubhama@policybazaar.com>
2019-02-27 12:01:11 +0530

    fix gtm

--- dev/js/helpers/gtm.js
+++ dev/js/helpers/gtm.js
@@ -59,19 +59,21 @@ const GTM = {
     },
 
     getVisitorInfo: () => {
-        let visitor_info = {}
+        let visitor_info = ""
+
         if (typeof window == "object") {
-            if(window.VISITOR_INFO){
-                visitor_info = window.VISITOR_INFO 
-            }
+            visitor_info = window.VISITOR_INFO
         }
-        if(!Object.values(visitor_info).length){
+
+        if (!visitor_info) {
             visitor_info = STORAGE.getVisitorInfo()
+        }
+
+        if (visitor_info && visitor_info.length) {
             visitor_info = JSON.parse(visitor_info)
         }
-        
 
-        if (visitor_info) {
+        if (visitor_info && visitor_info.visit_id && visitor_info.visitor_id) {
             let last_visit_difference = new Date().getTime() - visitor_info.last_visit_time;
 
             if (last_visit_difference > 1800000) {
@@ -90,11 +92,11 @@ const GTM = {
             }
         }
 
-        if(typeof window == "object"){
-            window.VISITOR_INFO = visitor_info
-        }
-
         let updated_cookie_val = JSON.stringify(visitor_info)
+
+        if (typeof window == "object") {
+            window.VISITOR_INFO = updated_cookie_val
+        }
         STORAGE.setVisitorInfo(updated_cookie_val)
 
         return visitor_info
