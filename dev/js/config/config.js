77d25aef714a51864f56468033501a8a053bd2e1
Arun Chaudhary <arunchaudhary@policybazaar.com>
2019-02-28 19:26:19 +0530

    fixes

--- dev/js/config/config.js
+++ dev/js/config/config.js
@@ -45,7 +45,7 @@ const STAGING_CONFIG = {
 
 const DEV_CONFIG = {
     // API_BASE_URL: "http://10.0.146.65:8000",
-    API_BASE_URL: "https://qa.docprime.com",
+    API_BASE_URL: "https://liveqa.docprime.com",
     // SOCKET_BASE_URL: "http://10.0.28.67:4444",
     SOCKET_BASE_URL: "https://liveqa.docprime.com",
     // SOCKET_BASE_URL: "http://localhost:4444",
