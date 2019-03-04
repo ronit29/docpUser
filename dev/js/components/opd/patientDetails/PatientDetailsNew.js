18c57355f247dc938041d6af9780a65416c9dcfc
rj_rishab28 <rishabsurana@docprime.com>
2019-03-04 16:18:42 +0530

    done test n doctor

--- dev/js/components/opd/patientDetails/PatientDetailsNew.js
+++ dev/js/components/opd/patientDetails/PatientDetailsNew.js
@@ -539,7 +539,7 @@ class PatientDetailsNew extends React.Component {
         if (!enabled_for_cod_payment && this.props.payment_type == 2) {
             this.props.select_opd_payment_type(1)
         }
-
+        console.log(patient)
         return (
             <div className="profile-body-wrap">
                 <ProfileHeader />
@@ -743,9 +743,13 @@ class PatientDetailsNew extends React.Component {
                                                             </div> : ""
                                                         }
 
-                                                        <div className="referral-select">
-                                                        <label className="ck-bx" style={{ fontWeight: '600', fontSize: '14px' }}>Enable WhatsApp notification<input type="checkbox" onClick={this.toggleWhatsap.bind(this)} checked={this.state.whatsapp_optin} /><span className="checkmark"></span></label>
-                                                        </div>
+                                                        {
+                                                            patient && patient.whatsapp_optin == null?
+                                                                <div className="referral-select">
+                                                                    <label className="ck-bx" style={{ fontWeight: '600', fontSize: '14px' }}>Enable WhatsApp notification<input type="checkbox" onClick={this.toggleWhatsap.bind(this)} checked={this.state.whatsapp_optin} /><span className="checkmark"></span></label>
+                                                                </div>
+                                                            :''
+                                                        }
 
                                                         <div className="lab-visit-time test-report" style={{ marginTop: 10, cursor: 'pointer', marginBottom: 0 }} onClick={this.toggle.bind(this, 'openCancellation')}>
                                                             <h4 className="title payment-amt-label fs-italic">Free Cancellation rishi<span style={{ marginLeft: 5 }}><img src={ASSETS_BASE_URL + "/img/icons/info.svg"} /></span></h4>
