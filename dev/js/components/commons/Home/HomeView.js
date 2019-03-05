e0b6f690bb65c3c1bda6d4383d2335c7ed0da3f4
rj_rishab28 <rishabsurana@docprime.com>
2019-03-05 13:47:07 +0530

    D

--- dev/js/components/commons/Home/HomeView.js
+++ dev/js/components/commons/Home/HomeView.js
@@ -196,6 +196,27 @@ class HomeView extends React.Component {
 				<div className="col-md-5">
 					<div className="right-card-container">
 
+						<div className="card cstm-card aptmnt-card mb-3">
+                            <div className="float-l leftBlock">
+                                <div className="float-l">
+                                    <img src={ASSETS_BASE_URL + "/images/ic-time-date.png"} alt="" />
+                                </div>
+                                <div className="float-l">
+                                    <div>Appointment for Ravi singh</div>
+                                    <div className="date-time">On 18 Feb, 11:12 AM</div>
+                                    <div> with Dr. Anil Vardani</div>
+                                </div>
+                            </div>
+                            <div className="rightBlock">
+                                <a href="javascript:void(0);" className="anchr-img">View Detail</a>
+                            </div>
+                            <div className="carousel-indicators crsl-indicators cr-indicator">
+                                <span className="active"></span>
+                                <span className=""></span>
+                                <span className=""></span>
+                            </div>
+                        </div>
+
 						<HomePageWidget
 							heading="Find a Doctor"
 							discount="50%"
