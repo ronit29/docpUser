334fbc7282f79d464306efda2f0c7c584348f373
Mayank <mayankyadav403@gmail.com>
2019-03-27 12:19:56 +0530

    ui fix updated

--- dev/js/components/opd/commons/selectedClinic/SelectedClinic.js
+++ dev/js/components/opd/commons/selectedClinic/SelectedClinic.js
@@ -47,14 +47,14 @@ class SelectedClinic extends React.Component {
 
         return (
             <div className="widget mrb-15 mrng-top-12">
-                <div className="widget-header dr-qucik-info" style={this.props.history ? { cursor: 'pointer' } : {}} onClick={() => this.profileClick(id, url, hospital_id)}>
+                <div className="widget-header dr-qucik-info">
                     <div>
-                        <InitialsPicture name={name} has_image={!!thumbnail} className="initialsPicture-dp">
-                            <img src={thumbnail} className="img-fluid img-round" />
+                        <InitialsPicture name={name} has_image={!!thumbnail} className="initialsPicture-dp docCardIntPic">
+                            <img src={thumbnail} style={{width:'60px', height:'60px'}} className="img-fluid img-round" />
                         </InitialsPicture>
                     </div>
-                    <div className="dr-profile mrt-10">
-                        <h1 className="dr-name">{display_name}</h1>
+                    <div className="dr-profile" onClick={() => this.profileClick(id, url, hospital_id)}>
+                        <h1 className="dr-name">{display_name}<span className="nwDocViewPrf" >(View Profile)</span></h1>
                         <p className="clinic-name text-sm">{hospitalName}</p>
                     </div>
                 </div>
