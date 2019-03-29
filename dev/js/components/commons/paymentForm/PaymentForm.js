db8d1742f2c76fb7a183a5f3c1c3790f96fccb90
prince kumar <princek@policybazaar.com>
2019-03-29 15:57:24 +0530

    rm payment option for other cases

--- dev/js/components/commons/paymentForm/PaymentForm.js
+++ dev/js/components/commons/paymentForm/PaymentForm.js
@@ -38,7 +38,12 @@ class PaymentForm extends React.Component {
                     <input type="text" name="hash" value={this.props.paymentData['hash']} />
                     <input type="text" name="gateway" value={this.props.gateway || 'paytm'} />
                     <input type="text" name="mode" value={this.props.mode} />
-                    <input type="text" name="insurerCode" value={this.props.paymentData['insurerCode']?this.props.paymentData['insurerCode']:''} />
+                    {
+                        this.props.paymentData && this.props.paymentData['insurerCode']?
+                        <input type="text" name="insurerCode" value={this.props.paymentData['insurerCode']} />
+                        :''    
+                    }
+                    
                 </form>
 
 
