27796aa9a4c73b1606c9692d363a836304ab8248
rishab <rishabsurana@docprime.com>
2019-04-24 15:45:07 +0530

    D

--- dev/js/components/diagnosis/searchPackages/packageCompare/packageCompareView.js
+++ dev/js/components/diagnosis/searchPackages/packageCompare/packageCompareView.js
@@ -35,29 +35,30 @@ const queryString = require('query-string');
       if (window) {
           window.scrollTo(0, 0)
       }
-      // let ids = []
-      // if(this.props.data.packages){
-      //   this.props.data.packages.map((packages, i) => {
-      //       ids.push('hide_av_'+ packages.id)
-      //       ids.push('hide_strt_'+ packages.id)
-      //       // ids.push('hide_coupon_'+ packages.id)
-      //   })
-      // }
-
-      // if (ids.length > 0) {
-      //     window.onscroll = function() {
-      //       if(document.getElementsByClassName('sticky-multiple-pkgs') && document.getElementsByClassName('sticky-multiple-pkgs')[0]){
-      //         let scrollHeight = document.getElementsByClassName('sticky-multiple-pkgs')[0].offsetTop
-      //         ids.map((id,i)=>{
-      //           if (scrollHeight >0 && window.screen.width < 768) {
-      //             document.getElementById(id).classList.add("d-none")
-      //           } else {
-      //             document.getElementById(id).classList.remove("d-none")
-      //           }
-      //         }) 
-      //       }
-      //     }
-      // }
+      let ids = []
+      if(this.props.data.packages){
+        this.props.data.packages.map((packages, i) => {
+            ids.push('hide_av_'+ packages.id+'_'+packages.lab.id)
+            // ids.push('hide_strt_'+ packages.id+'_'+packages.lab.id)
+            // ids.push('hide_coupon_'+ packages.id)
+        })
+      }
+      if (ids.length > 0) {
+          window.onscroll = function() {
+            if(document.getElementsByClassName('sticky-multiple-pkgs') && document.getElementsByClassName('sticky-multiple-pkgs')[0]){
+              let scrollHeight = document.getElementsByClassName('sticky-multiple-pkgs')[0].offsetTop
+              ids.map((id,i)=>{
+                if (scrollHeight >0 && window.screen.width < 768) {
+                  document.getElementById('showDiff').classList.add("d-none")
+                  document.getElementById(id).classList.add("d-none")
+                } else {
+                  document.getElementById('showDiff').classList.remove("d-none")
+                  document.getElementById(id).classList.remove("d-none")
+                }
+              }) 
+            }
+          }
+      }
 
     }
 
@@ -246,7 +247,7 @@ const queryString = require('query-string');
                     <div className="container-fluid pad-all-0">
                       
                       <div className="sticky-multiple-pkgs">
-                        <div className="multi-pkg-cmpre">
+                        <div className="multi-pkg-cmpre" id="showDiff">
                           <div className="tgle-btn">
                             <label className="switch">
                               <span className="tgle-btn-txt"> Show Difference</span>
@@ -271,7 +272,7 @@ const queryString = require('query-string');
                                     
                                       <div className="pkg-hd">{packages.name}</div>
                                       {/*<div className="pkg-hd-by" id={"hide_av_" + packages.id}>Available in {packages.total_labs_available} Labs</div>*/}
-                                      <div className="pkg-hd-by" id={"hide_av_" + packages.id}>{packages.lab.name}</div>
+                                      <div className="pkg-hd-by fw-500" id={"hide_av_" + packages.id+'_'+packages.lab.id}>{packages.lab.name}</div>
                                       
                                       <h3 className="lab-fltr-dc-name fw-500 pkg-include">{packages.total_parameters_count} Tests Included</h3>
                                       <div className="pkg-card-price">
