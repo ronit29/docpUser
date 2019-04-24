26a0c98b339e6fae097eca39e858d600b101cf58
rishab <rishabsurana@docprime.com>
2019-04-24 08:23:38 +0530

    D

--- dev/js/components/insurance/insuranceSelf.js
+++ dev/js/components/insurance/insuranceSelf.js
@@ -244,7 +244,7 @@ class InsuranceSelf extends React.Component{
             this.setState({ dateModal: false })
         }
     }
- //    handleState(event) {
+ 	// handleState(event) {
 	// 	var event = document.getElementById("state_dropdown")
 	// 	this.setState({state: event.options[event.selectedIndex].value, state_code: event.options[event.selectedIndex].id},() =>{
 	// 		this.handleSubmit(event)
@@ -320,101 +320,80 @@ class InsuranceSelf extends React.Component{
     }
 
  	autocomplete(inp, arr,type) {
-	    let self = this
-	    var currentFocus;
+		let self = this
+		var currentFocus;
 
-	     inp.addEventListener("input", function(e) {
-	      var a, b, i, val = this.value;
-	      /*close any already open lists of autocompleted values*/
-	      self.closeAllLists(type);
-	      if (!val) { return false;}
-	      currentFocus = -1;
-	      /*create a DIV element that will contain the items (values):*/
-	      a = document.createElement("DIV");
-	      a.setAttribute("id", this.id + "autocomplete-list");
-	      a.setAttribute("class", "autocomplete-items");
-	      /*append the DIV element as a child of the autocomplete container:*/
-	      this.parentNode.appendChild(a);
-	      /*for each item in the array...*/
-	      for (i = 0; i < arr.length; i++) {
-	        /*check if the item starts with the same letters as the text field value:*/
-	        if (arr[i].name.substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-	          /*create a DIV element for each matching element:*/
-	          b = document.createElement("DIV");
-	          /*make the matching letters bold:*/
-	          b.innerHTML = "<strong>" + arr[i].name.substr(0, val.length) + "</strong>";
-	          b.innerHTML += arr[i].name.substr(val.length);
-	          /*insert a input field that will hold the current array item's value:*/
-	          b.innerHTML += "<input type='hidden' value='" + arr[i].name + "' id='" + arr[i].code + "'>";
-	          /*execute a function when someone clicks on the item value (DIV element):*/
-	          b.addEventListener("click", function(e) {
-	              /*insert the value for the autocomplete text field:*/
-	            inp.value = this.getElementsByTagName("input")[0].value;
-	          		if(type == 'isState'){
-	     				self.setState({state:inp.value,state_code:this.getElementsByTagName("input")[0].id})
-					}else if(type == 'isDistrict'){
-						self.setState({district:inp.value,district_code:this.getElementsByTagName("input")[0].id})
-					}else if(type == 'isTown'){
-						self.setState({town:inp.value,town_code:this.getElementsByTagName("input")[0].id})
-					}
-	          		
-	     			self.handleSubmit();
-	              /*close the list of autocompleted values,
-	              (or any other open lists of autocompleted values:*/
-	              self.closeAllLists(type);
-	          });
-	          a.appendChild(b);
-	        }
-	      }
-	  	})
-		  /*execute a function presses a key on the keyboard:*/
-		  inp.addEventListener("keydown", function(e) {
-		      var x = document.getElementById(this.id + "autocomplete-list");
-		      if (x) x = x.getElementsByTagName("div");
-		      if (e.keyCode == 40) {
-		        /*If the arrow DOWN key is pressed,
-		        increase the currentFocus variable:*/
-		        currentFocus++;
-		        /*and and make the current item more visible:*/
-		        self.addActive(x);
-		      } else if (e.keyCode == 38) { //up
-		        /*If the arrow UP key is pressed,
-		        decrease the currentFocus variable:*/
-		        currentFocus--;
-		        /*and and make the current item more visible:*/
-		        self.addActive(x);
-		      } else if (e.keyCode == 13) {
-		        /*If the ENTER key is pressed, prevent the form from being submitted,*/
-		        e.preventDefault();
-		        if (currentFocus > -1) {
-		          /*and simulate a click on the "active" item:*/
-		          if (x) x[currentFocus].click();
-		        }
-		      }
-		  })
+		inp.addEventListener("input", function(e) {
+			var parentDiv, childDiv, i, val = this.value;
+
+			self.closeAllLists(type);
+			if (!val) { return false;}
+			currentFocus = -1;
+
+			parentDiv = document.createElement("DIV")
+			parentDiv.setAttribute("id", this.id + "autocomplete-list")
+			parentDiv.setAttribute("class", "autocomplete-items")
+			this.parentNode.appendChild(parentDiv)
+		 
+			for (i = 0; i < arr.length; i++) {
+				/*check if the item starts with the same letters as the text field value:*/
+				if (arr[i].name.substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+				  childDiv = document.createElement("DIV");
+				  childDiv.innerHTML = "<strong>" + arr[i].name.substr(0, val.length) + "</strong>";
+				  childDiv.innerHTML += arr[i].name.substr(val.length);
+				  childDiv.innerHTML += "<input type='hidden' value='" + arr[i].name + "' id='" + arr[i].code + "'>";
+
+				  childDiv.addEventListener("click", function(e) {
+				    inp.value = this.getElementsByTagName("input")[0].value;
+				  		if(type == 'isState'){
+								self.setState({state:inp.value,state_code:this.getElementsByTagName("input")[0].id})
+						}else if(type == 'isDistrict'){
+							self.setState({district:inp.value,district_code:this.getElementsByTagName("input")[0].id})
+						}else if(type == 'isTown'){
+							self.setState({town:inp.value,town_code:this.getElementsByTagName("input")[0].id})
+						}
+				  		
+				  	self.handleSubmit()
+				    self.closeAllLists(type)
+				  })
+				  parentDiv.appendChild(childDiv);
+				}
+			}
+		})
+		/*execute a function on key presses:*/
+		inp.addEventListener("keydown", function(e) {
+		  var x = document.getElementById(this.id + "autocomplete-list");
+		  if (x) x = x.getElementsByTagName("div");
+			  if (e.keyCode == 40) {
+			    currentFocus++;
+			    self.addActive(x);
+			  } else if (e.keyCode == 38) {
+			    currentFocus--;
+			    self.addActive(x);
+			  } else if (e.keyCode == 13) {
+			    e.preventDefault();
+			    if (currentFocus > -1) {
+			      if (x) x[currentFocus].click();
+			    }
+			  }
+		})
 	}
 
   	addActive(x) {
-	    /*a function to classify an item as "active":*/
 	    if (!x) return false;
-	    /*start by removing the "active" class on all items:*/
 	    this.removeActive(x);
 	    if (currentFocus >= x.length) currentFocus = 0;
 	    if (currentFocus < 0) currentFocus = (x.length - 1);
-	    /*add class "autocomplete-active":*/
 	    x[currentFocus].classList.add("autocomplete-active");
   	}
   
 	removeActive(x) {
-	    /*a function to remove the "active" class from all autocomplete items:*/
 	    for (var i = 0; i < x.length; i++) {
 	      x[i].classList.remove("autocomplete-active");
 	    }
 	}
 
 	closeAllLists(elmnt,type) {
-	    /*close all autocomplete lists in the document,
-	    except the one passed as an argument:*/
 	    let inp
 	    if(type == 'isState'){
 	    	inp = document.getElementById("userState")
