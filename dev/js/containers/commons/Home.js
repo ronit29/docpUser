5ffdce263566a2b2566827bc62a620dd0971f186
Kunal Batra <>
2019-04-26 13:38:20 +0530

    code fix

--- dev/js/containers/commons/Home.js
+++ dev/js/containers/commons/Home.js
@@ -36,7 +36,7 @@ class Home extends React.Component {
         // this.props.fetchHeatlhTip()
         // this.props.fetchArticles()
         if (!this.props.common_tests.length || !this.props.common_package.length || !this.props.specializations.length || (this.props.selectedLocation && this.props.selectedLocation.locality)) {
-            this.props.loadOPDCommonCriteria(this.props.selectedLocation.locality || 'Delhi')
+            this.props.loadOPDCommonCriteria(this.props.selectedLocation.locality)
             this.props.loadLabCommonCriterias()
         }
 
