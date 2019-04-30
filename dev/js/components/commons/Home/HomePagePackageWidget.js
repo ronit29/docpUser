d9413bea054c3ce73181ec0c7bf6ecebe48abfc7
Mayank <mayankyadav403@gmail.com>
2019-04-30 15:41:16 +0530

    slider uk

--- dev/js/components/commons/Home/HomePagePackageWidget.js
+++ dev/js/components/commons/Home/HomePagePackageWidget.js
@@ -22,7 +22,7 @@ class HomePagePackageWidget extends React.Component {
 
     render() {
         return (
-           <div className="pakg-slider-container">
+            <div className="pakg-slider-container">
                 <div className="pkgSliderHeading">
                     <h5>Book Health Packages</h5>
                     <span onClick={this.navigateTo.bind(this, this.props.navTo)}>View All</span>
@@ -30,20 +30,28 @@ class HomePagePackageWidget extends React.Component {
                 <div className="pkgSliderContainer">
                     {
                         this.props.list.map((listItem, i) => {
-                        return <div className="pkgcustCards" key={i} onClick={this.props.searchFunc.bind(this, listItem)}>
-                            {/*<span style={{fontSize:'10px'}} className="ofr-ribbon home-ofr-ribbon">{this.props.discount} Off</span>*/}
-                            <img className="img-fluid" src={listItem.icon} />
-                            <p className="pkgtstName">{listItem.name}</p>
-                            <p className="newpkgInclude">{listItem.no_of_tests} tests included </p>
-                            <p className="" style={{color:'green'}}>{parseInt(((listItem.mrp - listItem.agreed_price) / listItem.mrp) * 100)}% Off</p>
-                            <button className="pkgOutBtn">
-                                <p className="pkg-main-btn-prc">₹ {listItem.agreed_price}</p>
-                                <p className="pkg-main-btn-prc-cut">₹ {listItem.mrp}</p>
-                            </button>
-                        </div>
+                            return <div className="pkgcustCards" key={i} onClick={this.props.searchFunc.bind(this, listItem)}>
+                                {/*<span style={{fontSize:'10px'}} className="ofr-ribbon home-ofr-ribbon">{this.props.discount} Off</span>*/}
+                                <div className="pkgcardImgCont">
+                                    <img className="img-fluid" src={listItem.icon} />
+                                </div>
+                                <p className="pkgtstName">{listItem.name}</p>
+                                <p className="newpkgInclude">{listItem.no_of_tests} tests included </p>
+                                <p className="nwpkgofrpera" style={{ color: 'green' }}>{parseInt(((listItem.mrp - listItem.agreed_price) / listItem.mrp) * 100)}% Off</p>
+                                <button className="pkgOutBtn">
+                                    {/* <p className="pkg-main-btn-prc">₹ {listItem.agreed_price}</p>
+                                    <p className="pkg-main-btn-prc-cut">₹ {listItem.mrp}</p> */}
+                                    <p className="pkg-main-btn-prc">₹ 9900</p>
+                                    <p className="pkg-main-btn-prc-cut">₹ 8899</p>
+                                </button>
+                            </div>
                         })
                     }
                 </div>
+                <div className="pkglftRhtbtncont">
+                        <div className="pkg-btnlft"><img src={ASSETS_BASE_URL +"/img/customer-icons/dropdown-arrow.svg"} /></div>
+                        <div className="pkg-btnrgt"><img src={ASSETS_BASE_URL +"/img/customer-icons/dropdown-arrow.svg"} /></div>
+                    </div>
             </div>
         )
     }
