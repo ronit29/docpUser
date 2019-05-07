cd78b54e96aabc48222ade6a779cc625cb0ca9c1
rishab <rishabsurana@docprime.com>
2019-05-07 12:36:29 +0530

    done lab confirmation

---
+++ dev/js/components/diagnosis/bookingSummary/BookingConfirmationPopup.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+class BookingConfirmationPopup extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			
+		}
+	}
+
+	render() {
+		return (
+		<div className="search-el-popup-overlay " >
+			<div className="search-el-popup">
+				<div className="widget">
+					<div className="widget-content padiing-srch-el">
+						<p className="srch-el-conent">Are you sure you want to cancel your policy?</p>
+						<div className="search-el-btn-container">
+							<button onClick={this.props.priceConfirmationPopup.bind(this, true)}>Yes</button>
+							<span className="src-el-btn-border"></span>
+							<button onClick={this.props.priceConfirmationPopup.bind(this, false)}>No</button>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>)
+	}
+}
+
+export default BookingConfirmationPopup    
