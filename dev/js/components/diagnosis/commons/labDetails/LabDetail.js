242bb29fcf1b9bcdaa9236bd6e03fcddb4f96304
Kunal Batra <>
2019-05-14 19:05:47 +0530

    Lab page changes in case of insurance done

--- dev/js/components/diagnosis/commons/labDetails/LabDetail.js
+++ dev/js/components/diagnosis/commons/labDetails/LabDetail.js
@@ -147,11 +147,11 @@ class LabDetails extends React.Component {
                                     </ul>
                                     {
                                         STORAGE.isAgent() || ( !this.props.hide_price && !this.props.is_user_insured)?
-                                        <div className="serch-nw-inputs mb-0" onClick={this.openTests.bind(this)}>
-                                            <input type="text" autocomplete="off" className="d-block clkInput new-srch-doc-lab" id="search_bar" value="" placeholder="Search tests"/>
-                                            <img className="srch-inp-img" src="https://cdn.docprime.com/cp/assets/img/shape-srch.svg" style={{width: '15px'}}/>
-                                        </div>
-                                    :''
+                                            this.props.location && this.props.location.search && this.props.location.search.includes('from=insurance_network') ? "" :
+                                                <div className="serch-nw-inputs mb-0" onClick={this.openTests.bind(this)}>
+                                                    <input type="text" autocomplete="off" className="d-block clkInput new-srch-doc-lab" id="search_bar" value="" placeholder="Search tests"/>
+                                                    <img className="srch-inp-img" src="https://cdn.docprime.com/cp/assets/img/shape-srch.svg" style={{width: '15px'}}/>
+                                                </div> :''
                                     }
                                 </div>
 
