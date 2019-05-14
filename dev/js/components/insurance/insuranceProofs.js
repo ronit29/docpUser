c33e63880779d727cc912fcdab6a0d2814faa4a7
rishab <rishabsurana@docprime.com>
2019-05-14 22:39:27 +0530

    done

--- dev/js/components/insurance/insuranceProofs.js
+++ dev/js/components/insurance/insuranceProofs.js
@@ -6,7 +6,9 @@ class InsuranceProofs extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            dataUrl: null
+            dataUrl: null,
+            zoomImageUrl:null,
+            zoomImage:false
         }
     }
 
@@ -125,6 +127,20 @@ class InsuranceProofs extends React.Component {
         return new Blob([new Uint8Array(array)], { type: 'image/jpeg' });
     }
 
+    zoomImage(img){
+        this.setState({zoomImageUrl:img,zoomImage:true})
+        if(document.body){
+            document.body.style.overflow='hidden'
+        }
+    }
+
+    closeZoomImage(){
+        this.setState({zoomImage:false,zoomImageUrl:null})
+        if(document.body){
+            document.body.style.overflow=''
+        }
+    }
+
     render() {
         let Uploaded_image_data = []
         if (this.props.members_proofs && this.props.members_proofs.length > 0) {
@@ -147,17 +163,17 @@ class InsuranceProofs extends React.Component {
                         document.getElementById('imageFilePicker_' + this.props.member_id + '_front').click()
                         document.getElementById('imageFilePicker_' + this.props.member_id + '_front').value = ""
                     }}><img src={ASSETS_BASE_URL + "/img/ins-up-ico.svg"}/> Upload
-                        <input type="file" style={{ display: 'none' }} id={`imageFilePicker_${this.props.member_id}_front`} onChange={this.pickFile.bind(this, this.props.member_id, 'front')} />
+                        <input type="file" style={{ display: 'none' }} id={`imageFilePicker_${this.props.member_id}_front`} onChange={this.pickFile.bind(this, this.props.member_id, 'front')} accept="image/*"/>
                     </span>
                 :''}
             </div>
             {
                 Uploaded_image_data && Uploaded_image_data.length > 0 ?
                     <div className="upload-img-section">
-                        <img className="img-fluid ins-up-img-ic" src={Uploaded_image_data[0].front_img} style={{  }} />
+                        <img onClick={this.zoomImage.bind(this,Uploaded_image_data[0].front_img)} className="img-fluid ins-up-img-ic" src={Uploaded_image_data[0].front_img} style={{  }} />
                         {
                             Uploaded_image_data[0].back_img ?
-                                <img className="img-fluid ins-up-img-ic" src={Uploaded_image_data[0].back_img} style={{  }} /> : ''
+                                <img onClick={this.zoomImage.bind(this,Uploaded_image_data[0].back_img)} className="img-fluid ins-up-img-ic" src={Uploaded_image_data[0].back_img} style={{  }} /> : ''
                         }
                         {
                             Uploaded_image_data[0].back_img?''
@@ -167,11 +183,22 @@ class InsuranceProofs extends React.Component {
                             }}>
                                 <img className="ins-addico" src={ASSETS_BASE_URL + "/img/ins-add-ico.svg"} />
                                 Add More
-                                <input type="file" style={{ display: 'none' }} id={`imageFilePicker_${this.props.member_id}_back`} onChange={this.pickFile.bind(this, this.props.member_id, 'back')} />
+                                <input type="file" style={{ display: 'none' }} id={`imageFilePicker_${this.props.member_id}_back`} onChange={this.pickFile.bind(this, this.props.member_id, 'back')} accept="image/*"/>
                             </span>
                         }
                     </div>
                     : ''
+            }
+            {
+                this.state.zoomImage && this.state.zoomImageUrl?
+                <div className="search-el-popup-overlay" onClick={this.closeZoomImage.bind(this)}>
+                        <div className="search-el-popup">
+                            <div className="search-el-btn-container">
+                                 <img style={{maxHeight:'200px'}}src={this.state.zoomImageUrl}/>
+                            </div>
+                        </div>
+                </div>
+                :''
             }
         </div>
 
