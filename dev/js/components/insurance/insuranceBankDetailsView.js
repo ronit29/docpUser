0cfec3e418e4b21e5ef902bee18187a0a6b3f39c
Mayank <mayankyadav403@gmail.com>
2019-05-20 16:42:17 +0530

    cancel upload

--- dev/js/components/insurance/insuranceBankDetailsView.js
+++ dev/js/components/insurance/insuranceBankDetailsView.js
@@ -8,7 +8,7 @@ class InsuranceCancellationView extends React.Component {
 		super(props)
 		this.state = {
 			showCancelPopup: false,
-			showCancelSection:true
+			showCancelSection: true
 		}
 	}
 
@@ -33,23 +33,39 @@ class InsuranceCancellationView extends React.Component {
 			// 	}
 			// })
 		} else {
-			this.setState({ showCancelPopup: false})
+			this.setState({ showCancelPopup: false })
 		}
 	}
 
 	render() {
-			return <div className="profile-body-wrap" style={{ paddingBottom: 80 }} >
-				<ProfileHeader />
-				<section className="container parent-section book-appointment-section container-top-margin">
-					<div className="row main-row parent-section-row">
-						<div className="col-12 col-md-7 col-lg-7">
-							<section className="profile-book-screen">
-							</section>
-						</div>
-						<ChatPanel />
+		return <div className="profile-body-wrap" style={{ paddingBottom: 80 }} >
+			<ProfileHeader />
+			<section className="container parent-section book-appointment-section container-top-margin">
+				<div className="row main-row parent-section-row">
+					<div className="col-12 col-md-7 col-lg-7">
+						<section className="profile-book-screen">
+							<div className="widget">
+								<div className="widget-content">
+									<h1 className="ins-cancl-heading">We need bank account details to proceed with your cancellation</h1>
+									<div className="ins-cancl-container">
+										<input className="ins-cn-inp" type="text" placeholder="Account Holder Name" />
+										<input className="ins-cn-inp" type="text" placeholder="Bank Name" />
+										<textarea className="ins-cn-textarea" placeholder="Address"></textarea>
+										<input className="ins-cn-inp" type="number" placeholder="Account Number" />
+										<input className="ins-cn-inp" type="text" placeholder="IFSC Code" />
+									</div>
+									<p className="ins-cancl-para">We need to confirm if this account belongs to you. Please fill more details below </p>
+									<button className="ins-cn-btn"><img src={ASSETS_BASE_URL + '/img/upld.png'} />Upload Cancelled Cheque</button>
+									<p className="ins-cancl-para">OR</p>
+									<button className="ins-cn-btn"><img src={ASSETS_BASE_URL + '/img/upld.png'} />Upload Account Statement</button>
+								</div>
+							</div>
+						</section>
 					</div>
-				</section>
-			</div>
+					<ChatPanel />
+				</div>
+			</section>
+		</div>
 
 	}
 }
