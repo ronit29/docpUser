759e8966b4a7e8bd9a381061d0230938da39406c
Mayank <mayankyadav403@gmail.com>
2019-05-23 20:09:40 +0530

    ui update

--- dev/js/components/commons/ChatPanel/ChatIpdPanel.js
+++ dev/js/components/commons/ChatPanel/ChatIpdPanel.js
@@ -32,15 +32,15 @@ class IpdChatPanel extends React.Component {
 
 			<div className={`ipd-chat-pop ${this.state.maximize?'ipd-chat-pop-full':this.state.minimize?'ipd-chat-pop-minimize':''}`} >
 				<div className="ipd-chat-header">
-					<p onClick={()=>this.setState({'maximize': true})}>Need help in bookin doctor appointment/surgery?</p>
+					<p onClick={()=>this.setState({maximize: true, minimize: false})}>Need help in bookin doctor appointment/surgery?</p>
 					<div className="cht-head-rqst-btn" >
 						{
 							this.state.minimize?
 							<span  onClick={()=>this.closeChat()}>
-								<img className="close-chat" src="https://cdn.docprime.com/cp/assets/img/chatminimize.svg" style={{ width: '20px' }} />
+								<img className="close-chat" src={ASSETS_BASE_URL +'/img/customer-icons/close-black.svg'} style={{ width: '11px',display:'block', lineHeight:'0' }} />
 							</span>:
-							<span  onClick={()=>this.setState({minimize: true, maximize: !this.state.maximize})}>
-								<img className="close-chat" src="https://cdn.docprime.com/cp/assets/img/chatminimize.svg" style={{ width: '20px' }} />
+							<span  onClick={()=>this.setState({minimize: true, maximize: false})}>
+								<img className="close-chat" src={ASSETS_BASE_URL +'/img/chatminimize.svg'} style={{ width: '20px' }} />
 							</span>	
 						}
 						
