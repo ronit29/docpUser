25860e6d10f93ee7e6c9695d29a72ecde79ea3fe
Kunal Batra <>
2019-05-30 16:14:21 +0530

    chat widget added on home page and UI of bottom fixed footer updated

--- dev/js/components/commons/Home/FixedMobileFooter.js
+++ dev/js/components/commons/Home/FixedMobileFooter.js
@@ -149,9 +149,9 @@ class FixedMobileFooter extends React.Component {
                     }}>
                         <img className="footbgRound" src={ASSETS_BASE_URL + "/img/chatFoot_new.svg"} />
                         <div className="nw-mid-container-with-img" style={{ zIndex: '1', color: 'white' }}>
-                            <span style={{ fontSize: 11 }}>Free Online</span>
+                            {/* <span style={{ fontSize: 11 }}>Free Online</span> */}
                             <div className="">
-                                <img style={{ width: 30, position:'relative', top: '-5px' }} src={ASSETS_BASE_URL + "/img/chatbt_new.svg"} />
+                                <img style={{ width: 34, position:'relative', top: '-5px' }} src={ASSETS_BASE_URL + "/img/chatbt_new.svg"} />
                             </div>
                             <span style={{ fontSize: 11 }}>Doctor Consult</span>
                         </div>
