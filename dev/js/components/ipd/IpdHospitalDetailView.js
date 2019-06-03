a7322766edd344d4559a99c830a4cc6b0c2cdb7e
Mayank <mayankyadav403@gmail.com>
2019-06-03 12:35:12 +0530

    ipd offers

--- dev/js/components/ipd/IpdHospitalDetailView.js
+++ dev/js/components/ipd/IpdHospitalDetailView.js
@@ -269,30 +269,38 @@ class HospitalDetailView extends React.Component {
 							</div>
 
 							<div className="hs-card">
-								<div className="widget-content">
-									<h4 className="hs-ofr-heading"><img src={ASSETS_BASE_URL + '/img/ofr-cpn.svg'} />Offers Available</h4>
-									<div className="hs-offerCard">
-										<div className="hs-ofr-crdHeading">
-											<p><img src={ASSETS_BASE_URL + '/img/ofr-cpn.svg'} />₹51000 OFF on Maternity</p>
-											<img src={ASSETS_BASE_URL + '/img/ofr-cpn.svg'} />
-										</div>
-										<div className="hs-ofr-card-content">
-											<p>
-												Lorem ipsum dolor sit amet, consectetur adipiscing elit
+								<div className="ipd-ofr-cont">
+									<div className="widget-content">
+									<h4 className="hs-ofr-heading"><img src={ASSETS_BASE_URL + '/img/ipd-ofr.svg'} />Offers Available</h4>
+										<div className="ipd-ofr-main">
+											<div className="hs-offerCard">
+												<div className="hs-ofr-crdHeading">
+													<p><img src={ASSETS_BASE_URL + '/img/ipd-cpn.svg'} />₹51000 OFF on Maternity</p>
+													<img src={ASSETS_BASE_URL + '/img/ipd-share.svg'} />
+												</div>
+												<div className="hs-ofr-card-content">
+													<div className="cpn-rqrd">
+														<p>User Promo code :  <span>DOC50</span></p>
+													</div>
+													<p>
+														Lorem ipsum dolor sit amet, consectetur adipiscing elit
 										</p>
-											<p className="no-cpn-rqrd">No Coupon Required</p>
+													<p className="no-cpn-rqrd">No Coupon Required</p>
+												</div>
+												<div className="offer-hide-content">
+													<ul>
+														<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</li>
+														<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</li>
+														<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</li>
+													</ul>
+												</div>
+												<div className="hs-offer-footer">
+													<p className="tc_cont">T&C Apply</p>
+													<p className="show-hide-offr">Hide Details <img className="offshowIcon ofhideIcon " style={{ width: '7px', marginLeft: '5px' }} src={ASSETS_BASE_URL + '/img/right-sc.svg'} /></p>
+												</div>
+											</div>
 										</div>
-										<div className="offer-hide-content">
-											<ul>
-												<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</li>
-												<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</li>
-												<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</li>
-											</ul>
-										</div>
-										<div className="hs-offer-footer">
-											<p className="tc_cont">T&C Apply</p>
-											<p className="show-hide-offr">Hide Details <img style={{width:'18px'}} src={ASSETS_BASE_URL + '/img/ofr-cpn.svg'} /></p>
-										</div>
+										<p className="ofr-vw-more">View 6 more offers</p>
 									</div>
 								</div>
 							</div>
