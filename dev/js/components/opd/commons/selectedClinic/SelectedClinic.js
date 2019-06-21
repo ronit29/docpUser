d06e32d4356d786ccf4c8659e6847fcd000be506
Mayank <mayankyadav403@gmail.com>
2019-06-21 13:49:36 +0530

    ui done

--- dev/js/components/opd/commons/selectedClinic/SelectedClinic.js
+++ dev/js/components/opd/commons/selectedClinic/SelectedClinic.js
@@ -48,14 +48,57 @@ class SelectedClinic extends React.Component {
         return (
             <div className="widget mrb-15 mrng-top-12">
                 <div className="widget-header dr-qucik-info">
-                    <div onClick={() => this.profileClick(id, url, hospital_id)} style={{cursor:'pointer'}}>
+                    <div onClick={() => this.profileClick(id, url, hospital_id)} style={{ cursor: 'pointer' }}>
                         <InitialsPicture name={name} has_image={!!thumbnail} className="initialsPicture-dp docCardIntPic">
-                            <img src={thumbnail} style={{width:'60px', height:'60px'}} className="img-fluid img-round"/>
+                            <img src={thumbnail} style={{ width: '60px', height: '60px' }} className="img-fluid img-round" />
                         </InitialsPicture>
                     </div>
                     <div className="dr-profile">
                         <h1 className="dr-name">{display_name}<span className="nwDocViewPrf" onClick={() => this.profileClick(id, url, hospital_id)}>(View Profile)</span></h1>
-                        <p className="clinic-name text-sm">{hospitalName}</p>
+                        <span className="clinic-name text-sm">{hospitalName}</span>
+                        <span className="nw-clinicMore">+ 2 more Clinics <img src={ASSETS_BASE_URL + '/img/right-sc.svg'} /></span>
+                    </div>
+                </div>
+                <div className="clinicRadioContainer">
+                    <div className="dtl-radio">
+                        <label className="container-radio m-0">
+                            <div className="clinic-names-nw">
+                                <p className="clnc-name">
+                                    Dr. Satish Kumar Gadis Clinic
+                                </p>
+                                <p className="clnc-pricing-cont"><span className="clinc-rd-price">₹ 599 </span><span className="clinc-rd-price-cut">₹ 699</span></p>
+                            </div>
+                            <p className="clck-loc">Sector 44, Gurgaon</p>
+                            <input type="radio" name="gender" value='o' data-param='gender' checked />
+                            <span className="doc-checkmark"></span>
+                        </label>
+                    </div>
+                    <div className="dtl-radio">
+                        <label className="container-radio m-0">
+                            <div className="clinic-names-nw">
+                                <p className="clnc-name">
+                                    Dr. Satish Kumar Gadis Clinic
+                                </p>
+                                <p className="clnc-pricing-cont"><span className="clinc-rd-price">₹ 599 </span><span className="clinc-rd-price-cut">₹ 699</span></p>
+                            </div>
+                            <p className="clck-loc">Sector 44, Gurgaon</p>
+                            <input type="radio" name="gender" value='o' data-param='gender' checked />
+                            <span className="doc-checkmark"></span>
+                        </label>
+                    </div>
+                    <div className="dtl-radio">
+                        <label className="container-radio m-0">
+                            <div className="clinic-names-nw">
+                                <p className="clnc-name">
+                                    Dr. Satish Kumar Gadis Clinic
+                                </p>
+                                <p className="clnc-pricing-cont"><span className="clinc-rd-price">₹ 0 </span>
+                                </p>
+                            </div>
+                            <p className="clck-loc">Sector 44, Gurgaon <span>Covered under insurance</span></p>
+                            <input type="radio" name="gender" value='o' data-param='gender' checked />
+                            <span className="doc-checkmark"></span>
+                        </label>
                     </div>
                 </div>
             </div>
