cce9c4f2f658985399132fce9093c8a7f3ab8f1b
rishab <rishabsurana@docprime.com>
2019-06-24 18:17:22 +0530

    D

--- dev/js/components/commons/ratingsProfileView/RatingsPopUp.js
+++ dev/js/components/commons/ratingsProfileView/RatingsPopUp.js
@@ -130,7 +130,7 @@ class RatingsPopUp extends React.Component {
             let qualification_object = this.state.data.doctor ? this.state.data.doctor.qualifications : null;
             let pipe = ''
             let data_obj = {
-                'name': (this.state.data.doctor) ? this.state.data.doctor.name : this.state.data.lab.name,
+                'name': this.state.data && this.state.data.doctor ? this.state.data.doctor.name : this.state.data && this.state.data.lab ?this.state.data.lab.name:'',
                 'qualification': qualification_object && qualification_object.length ? qualification_object[0].qualification : '',
                 'specialization': qualification_object && qualification_object.length ? qualification_object[0].specialization : '',
                 'type': this.getAppointmentType(),
