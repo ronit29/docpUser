ed90fb3bf81aed9a93f802a10855fbce33fb18e4
Mayank <mayankyadav403@gmail.com>
2019-06-27 14:18:06 +0530

    ui update

--- dev/js/components/insurance/insuranceStaticView.js
+++ dev/js/components/insurance/insuranceStaticView.js
@@ -26,10 +26,10 @@ class InsuranceStaticView extends React.Component {
 			showPopup: false,
 			shortURL: "",
 			isLead: 'proceed',
-			checkIdleTimeout:true,
+			checkIdleTimeout: true,
 			popupClass: '',
 			overlayClass: '',
-			identifyUserClick:''
+			identifyUserClick: ''
 		}
 	}
 	componentDidMount() {
@@ -40,17 +40,17 @@ class InsuranceStaticView extends React.Component {
 		let phoneNumber = ''
 		if (!STORAGE.checkAuth() && lead_data.page_source == 'banner') {
 			// this.setState({checkIdleTimeout:false, showPopup:true, popupClass: 'translucent-popup', overlayClass: 'white-overlay', identifyUserClick:'bannerClick'})
-			this.setState({checkIdleTimeout:false, showPopup:true, popupClass: '', overlayClass: '', identifyUserClick:'bannerClick'})
-		let data = {
+			this.setState({ checkIdleTimeout: false, showPopup: true, popupClass: '', overlayClass: '', identifyUserClick: 'bannerClick' })
+			let data = {
 				'Category': 'ConsumerApp', 'Action': 'InsuranceLoginPopup', 'CustomerID': GTM.getUserId() || '', 'event': 'Insurance-login-popup-click', 'click_value': 'bannerClick'
 			}
 
-		GTM.sendEvent({ data: data })
+			GTM.sendEvent({ data: data })
 		}
 		if (STORAGE.checkAuth() && this.props.USER && this.props.USER.primaryMobile != '') {
-            phoneNumber = this.props.USER.primaryMobile
-        }
-        this.props.generateInsuranceLead('',phoneNumber,lead_data,this.props.selectedLocation)
+			phoneNumber = this.props.USER.primaryMobile
+		}
+		this.props.generateInsuranceLead('', phoneNumber, lead_data, this.props.selectedLocation)
 		let selectedId = this.props.selected_plan ? this.props.selected_plan.id : ''
 		if (selectedId) {
 			this.selectPlan(this.props.selected_plan)
@@ -60,36 +60,36 @@ class InsuranceStaticView extends React.Component {
 				this.textInput.click()
 			}
 		}
-		if(this.state.checkIdleTimeout && !STORAGE.checkAuth()){
+		if (this.state.checkIdleTimeout && !STORAGE.checkAuth()) {
 			// this.setState({popupClass: 'translucent-popup', overlayClass: 'white-overlay'})
 			this.inactivityTime()
 		}
 	}
 
 	inactivityTime() {
-    var time;
-    let self =  this
-    window.onload = resetTimer;
-    document.onmousemove = resetTimer;
-    document.onkeypress = resetTimer;
-    resetTimer()
-	    function stop() {
-	    	let data = {
+		var time;
+		let self = this
+		window.onload = resetTimer;
+		document.onmousemove = resetTimer;
+		document.onkeypress = resetTimer;
+		resetTimer()
+		function stop() {
+			let data = {
 				'Category': 'ConsumerApp', 'Action': 'InsuranceLoginPopup', 'CustomerID': GTM.getUserId() || '', 'event': 'Insurance-login-popup-click', 'click_value': 'AutoClick'
-				}
-				GTM.sendEvent({ data: data })
-	        if(document.getElementById('proceedLead')){
-	        	document.getElementById('proceedLead').click()
-	        	self.setState({checkIdleTimeout:false, identifyUserClick:'AutoClick'})
-	        }
-	    }
+			}
+			GTM.sendEvent({ data: data })
+			if (document.getElementById('proceedLead')) {
+				document.getElementById('proceedLead').click()
+				self.setState({ checkIdleTimeout: false, identifyUserClick: 'AutoClick' })
+			}
+		}
 
-	    function resetTimer() {
-	        clearTimeout(time);
-	        if(self.state.checkIdleTimeout){
-	        	time = setTimeout(stop, 5000)	
-	        }
-	    }
+		function resetTimer() {
+			clearTimeout(time);
+			if (self.state.checkIdleTimeout) {
+				time = setTimeout(stop, 5000)
+			}
+		}
 	}
 
 	componentWillReceiveProps(props) {
@@ -117,7 +117,7 @@ class InsuranceStaticView extends React.Component {
 		this.setState({ is_checked: plan_to_toggle.id, selected_plan_data: plan_to_toggle, selected_plan_price: `(₹ ${plan_to_toggle.amount})`, toggle: this.state.toggle == 'two' ? 'one' : 'one' })
 	}
 
-	proceedPlan(){
+	proceedPlan() {
 		let parsed = queryString.parse(this.props.location.search)
 		let plan = Object.assign({}, this.state.selected_plan_data)
 		let lead_data
@@ -128,10 +128,10 @@ class InsuranceStaticView extends React.Component {
 			}
 			if (Object.keys(plan).length > 0) {
 				lead_data = parsed
-				this.props.generateInsuranceLead(plan.id, phoneNumber,lead_data,this.props.selectedLocation)
+				this.props.generateInsuranceLead(plan.id, phoneNumber, lead_data, this.props.selectedLocation)
 			}
 			this.props.history.push('/insurance/insurance-plan-view')
-		}else{
+		} else {
 			this.setState({ showPopup: true })
 		}
 	}
@@ -158,7 +158,7 @@ class InsuranceStaticView extends React.Component {
 			}
 			if (Object.keys(plan).length > 0) {
 				lead_data = parsed
-				this.props.generateInsuranceLead(plan.id, phoneNumber,lead_data,this.props.selectedLocation)
+				this.props.generateInsuranceLead(plan.id, phoneNumber, lead_data, this.props.selectedLocation)
 			}
 			profileLength = Object.keys(this.props.USER.profiles).length
 			memberStoreDataLength = Object.keys(this.props.self_data_values).length
@@ -218,17 +218,17 @@ class InsuranceStaticView extends React.Component {
 	}
 
 	proceedLead(type) {
-		if(!this.state.checkIdleTimeout){
+		if (!this.state.checkIdleTimeout) {
 			let data = {
-				'Category': 'ConsumerApp', 'Action': 'InsuranceLoginPopup', 'CustomerID': GTM.getUserId() || '', 'event': 'Insurance-login-popup-click', 'click_value': 'userClick', type:type
+				'Category': 'ConsumerApp', 'Action': 'InsuranceLoginPopup', 'CustomerID': GTM.getUserId() || '', 'event': 'Insurance-login-popup-click', 'click_value': 'userClick', type: type
 			}
-			this.setState({popupClass: '', overlayClass: ''})
+			this.setState({ popupClass: '', overlayClass: '' })
 			GTM.sendEvent({ data: data })
 		}
-		this.setState({ isLead: type, showPopup: true, identifyUserClick:'userClick' })
+		this.setState({ isLead: type, showPopup: true, identifyUserClick: 'userClick' })
 	}
 
-	closeLeadPopup(){
+	closeLeadPopup() {
 		this.setState({ showPopup: false })
 	}
 
@@ -257,20 +257,20 @@ class InsuranceStaticView extends React.Component {
 		// 		<div className="profile-body-wrap">
 		// 			<ProfileHeader showPackageStrip={true}/>
 		// 			<HelmetTags tagsData={{
-  //                   	canonicalUrl: `${CONFIG.API_BASE_URL}/insurance/insurance-plans`,
-  //                   	title: 'OPD Insurance | Buy OPD Insurance Cover | OPD Cover',
-  //                   	description: 'OPD Insurance: Buy OPD insurance cover & get cashless benefits on lab tests & doctor consultation with a network of over 15000 doctors and 2000 labs.'
-  //               	}} noIndex={false} />                
+		//                   	canonicalUrl: `${CONFIG.API_BASE_URL}/insurance/insurance-plans`,
+		//                   	title: 'OPD Insurance | Buy OPD Insurance Cover | OPD Cover',
+		//                   	description: 'OPD Insurance: Buy OPD insurance cover & get cashless benefits on lab tests & doctor consultation with a network of over 15000 doctors and 2000 labs.'
+		//               	}} noIndex={false} />                
 		// 			<section className="container container-top-margin cardMainPaddingRmv">
 		// 				<div className="row no-gutters dsktp-row-gutter">
 		// 					<div className="col-12 col-md-7 col-lg-7 ins-main-padding">
 		// 						<section className="profile-book-screen" style={{position:'relative'}}>
 		// 							<div>
 		// 								{/*<div>
-  //                                   <span style={{ cursor: 'pointer' }} onClick={this.shortenUrl.bind(this)}>
-  //                                       <img src={ASSETS_BASE_URL + "/img/customer-icons/url-short.svg"} style={{ width: 80 }} />
-  //                                   </span>
-  //                               </div>*/}
+		//                                   <span style={{ cursor: 'pointer' }} onClick={this.shortenUrl.bind(this)}>
+		//                                       <img src={ASSETS_BASE_URL + "/img/customer-icons/url-short.svg"} style={{ width: 80 }} />
+		//                                   </span>
+		//                               </div>*/}
 		// 								{
 		// 									this.state.shortURL ? <div className="shareLinkpopupOverlay" onClick={() => {
 		// 										this.setState({ shortURL: "" })
@@ -377,116 +377,117 @@ class InsuranceStaticView extends React.Component {
 		// }
 		if (this.props.LOAD_INSURANCE) {
 			return (
-			<div>
-				<div className="profile-body-wrap">
-					<ProfileHeader showPackageStrip={true} />
+				<div>
+					<div className="profile-body-wrap">
+						<ProfileHeader showPackageStrip={true} />
 
-					<section className="container article-container">
-						<div className="row main-row parent-section-row justify-content-center">
-							<div className="col-12 col-md-10 col-lg-10 center-column">
-								<div className="container-fluid article-column">
-									<div className="ins-landing-container">
-										<div>
-											<div className="ins-steps-section">
-												<div className="doc-usr-only">
-													<h1>Group OPD Insurance exclusively <br></br>for <span>Docprime</span> users only </h1>
-													<div className="doc-pwdby">
-														<p>Powered by</p>
-														<img style={{ width: '130px' }} src="https://cdn.docprime.com/media/insurer/images/AMHI_Logo-01.png" />
-													</div>
-												</div>
-											</div>
-											<div className="ins-steps-section">
-												<div className="row align-item-center">
-													<div className="col-6">
-														<div className="lft-cntn-algn">
-															<img className="ins-step-img" src={ASSETS_BASE_URL + "/img/ins-hosp.png"} />
-														</div>
-													</div>
-													<div className="col-6">
-														<div className="lft-para-algn">
-															<p className="step-blk-para">Unlimited</p>
-															<p className="step-blk-para">Doctor</p>
-															<p className="step-orng-para">Consultation</p>
-															<p className="step-qut-para">with <span>20,000+</span> Doctors*</p>
+						<section className="container article-container">
+							<div className="row main-row parent-section-row justify-content-center">
+								<div className="col-12 col-md-10 col-lg-10 center-column">
+									<div className="container-fluid article-column ins-landing-shadow">
+										<div className="ins-landing-container">
+											<div>
+												<div className="ins-steps-section">
+													<div className="doc-usr-only">
+														<h1>Group OPD Insurance exclusively <br></br>for <span>Docprime</span> users only </h1>
+														<div className="doc-pwdby">
+															<p>Powered by</p>
+															<img style={{ width: '130px' }} src="https://cdn.docprime.com/media/insurer/images/AMHI_Logo-01.png" />
 														</div>
 													</div>
 												</div>
-											</div>
-											<div className="ins-steps-section">
-												<div className="row align-item-center">
-													<div className="col-6">
-														<div className="lft-para-algn">
-															<p className="step-blk-para">Unlimited</p>
-															<p className="step-orng-para">Lab Tests</p>
-															<p className="step-qut-para">with <span>20,000+</span> Doctors*</p>
+												<div className="ins-steps-section">
+													<div className="row align-item-center">
+														<div className="col-6">
+															<div className="lft-cntn-algn">
+																<img className="ins-step-img" src={ASSETS_BASE_URL + "/img/ins-hosp.png"} />
+															</div>
 														</div>
-													</div>
-													<div className="col-6">
-														<img className="ins-step-img" src={ASSETS_BASE_URL + "/img/isn-lab.png"} />
-													</div>
-												</div>
-											</div>
-											<div className="ins-steps-section">
-												<div className="row align-item-center">
-													<div className="col-6">
-														<div className="lft-cntn-algn">
-															<img className="ins-step-img" src={ASSETS_BASE_URL + "/img/ins-act.png"} />
-														</div>
-													</div>
-													<div className="col-6">
-														<div className="lft-para-algn">
-															<p className="step-orng-para">Instant activation</p>
-															<p className="step-blk-para">upon purchase</p>
+														<div className="col-6">
+															<div className="lft-para-algn">
+																<p className="step-blk-para">Unlimited</p>
+																<p className="step-blk-para">Doctor</p>
+																<p className="step-orng-para">Consultation</p>
+																<p className="step-qut-para">with <span>20,000+</span> Doctors*</p>
+															</div>
 														</div>
 													</div>
 												</div>
+												<div className="ins-steps-section">
+													<div className="row align-item-center">
+														<div className="col-6">
+															<div className="lft-para-algn">
+																<p className="step-blk-para">Unlimited</p>
+																<p className="step-orng-para">Lab Tests</p>
+																<p className="step-qut-para">with <span>20,000+</span> Doctors*</p>
+															</div>
+														</div>
+														<div className="col-6">
+															<img className="ins-step-img" src={ASSETS_BASE_URL + "/img/isn-lab.png"} />
+														</div>
+													</div>
+												</div>
+												<div className="ins-steps-section">
+													<div className="row align-item-center">
+														<div className="col-6">
+															<div className="lft-cntn-algn">
+																<img className="ins-step-img" src={ASSETS_BASE_URL + "/img/ins-act.png"} />
+															</div>
+														</div>
+														<div className="col-6">
+															<div className="lft-para-algn">
+																<p className="step-orng-para">Instant activation</p>
+																<p className="step-blk-para">upon purchase</p>
+															</div>
+														</div>
+													</div>
+												</div>
 											</div>
-										</div>
-										<div className="ins-land-listing">
-											<p><img src={ASSETS_BASE_URL + '/img/ins-hrt.png'} />Pre existing diseases covered</p>
-											<p><img src={ASSETS_BASE_URL + '/img/ins-rprt.png'} />No medical test required for policy insurance</p>
-											<p><img src={ASSETS_BASE_URL + '/img/ins-yr.png'} />Valid for 1 year from policy insurance</p>
-										</div>
-										<div className="ins-network-cont">
-											<div className="ins-netwrk-heading-cont">
-												<p>*Only available on Docprime network </p>
-												<span href='/insurance/network' onClick={(e)=>{
-													e.preventDefault();
-													this.props.history.push('/insurance/network')
+											<div className="ins-land-listing">
+												<p><img src={ASSETS_BASE_URL + '/img/ins-hrt.png'} />Pre existing diseases covered</p>
+												<p><img src={ASSETS_BASE_URL + '/img/ins-rprt.png'} />No medical test required for policy insurance</p>
+												<p><img src={ASSETS_BASE_URL + '/img/ins-yr.png'} />Valid for 1 year from policy insurance</p>
+											</div>
+											<div className="ins-network-cont">
+												<div className="ins-netwrk-heading-cont">
+													<p>*Only available on Docprime network </p>
+													<span href='/insurance/network' onClick={(e) => {
+														e.preventDefault();
+														this.props.history.push('/insurance/network')
 													}}>View network</span>
+												</div>
+												<p className="ins-nw-note">A few things to note... </p>
+												<ul className="ins-nw-listing">
+													<li>All procedures (dental, daycare etc..) and sessions (therapy, counselling etc..) are not covered</li>
+													<li>Any lab test or OPD appointments with MRP more than ₹1500 are not covered</li>
+													<li>Only 5 oncology and 5 Gynecology doctor appointments are covered in a policy year</li>
+													<li>Docprime's Insurance network is dynamic in nature and may change from time to time</li>
+												</ul>
 											</div>
-											<p className="ins-nw-note">A few things to note... </p>
-											<ul className="ins-nw-listing">
-												<li>All procedures (dental, daycare etc..) and sessions (therapy, counselling etc..) are not covered</li>
-												<li>Any lab test or OPD appointments with MRP more than ₹1500 are not covered</li>
-												<li>Only 5 oncology and 5 Gynecology doctor appointments are covered in a policy year</li>
-												<li>Docprime's Insurance network is dynamic in nature and may change from time to time</li>
-											</ul>
 										</div>
 									</div>
+									{parsed.show_button ?
+										<div className="sticky-btn fixed insuBtnsContainer">
+											<button className="insu-left-white-btn" onClick={() => this.props.history.go(-1)}>Back to Booking
+								</button>
+											<button className="insu-right-orng-btn" onClick={this.proceedPlan.bind(this)}>Buy Now
+								</button>
+										</div>
+										:
+										<div className="sticky-btn fixed insuBtnsContainer">
+											<button id="proceedLead" className="insu-right-orng-btn ins-buy-btn" onClick={this.proceedPlan.bind(this)}>Buy Now</button>
+										</div>
+									}
 								</div>
 							</div>
-						</div>
-						{parsed.show_button?
-							<div className="sticky-btn fixed insuBtnsContainer">
-								<button className="insu-left-white-btn" onClick={()=>this.props.history.go(-1)}>Back to Booking
-								</button>
-								<button className="insu-right-orng-btn" onClick={this.proceedPlan.bind(this)}>Buy Now 
-								</button>
-							</div>
-						:
-							<div className="sticky-btn fixed insuBtnsContainer">
-								<button id="proceedLead" className="insu-right-orng-btn ins-buy-btn" onClick={this.proceedPlan.bind(this)}>Buy Now</button>
-							</div>
+
+						</section>
+						{this.state.showPopup ?
+							<InsurPopup {...this.props} selected_plan={this.state.selected_plan_data} hideLoginPopup={this.hideLoginPopup.bind(this)} isLead={this.state.isLead} closeLeadPopup={this.closeLeadPopup.bind(this)} popupClass={this.state.popupClass} overlayClass={this.state.overlayClass} identifyUserClick={this.state.identifyUserClick} /> : ''
 						}
-					</section>
-					{this.state.showPopup ?
-						<InsurPopup {...this.props} selected_plan={this.state.selected_plan_data} hideLoginPopup={this.hideLoginPopup.bind(this)} isLead={this.state.isLead} closeLeadPopup={this.closeLeadPopup.bind(this)} popupClass={this.state.popupClass} overlayClass={this.state.overlayClass} identifyUserClick={this.state.identifyUserClick}/> : ''
-					}
+					</div>
 				</div>
-			</div>
-		)
+			)
 		}
 	}
 }
