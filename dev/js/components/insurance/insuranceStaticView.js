c30d10ebe3734d0576d93f5dd680218784910b25
rishab <rishabsurana@docprime.com>
2019-06-27 18:21:02 +0530

    D

--- dev/js/components/insurance/insuranceStaticView.js
+++ dev/js/components/insurance/insuranceStaticView.js
@@ -229,13 +229,15 @@ class InsuranceStaticView extends React.Component {
 	}
 
 	closeLeadPopup() {
-		this.setState({ showPopup: false })
+		this.setState({ showPopup: false, checkIdleTimeout:true },()=>{
+			this.inactivityTime()
+		})
 	}
 
 	hideLoginPopup() {
-		this.setState({
-			showPopup: false
-		});
+		this.setState({showPopup: false,checkIdleTimeout:true},()=>{
+			this.inactivityTime()
+		})
 	}
 
 	shortenUrl() {
@@ -251,130 +253,7 @@ class InsuranceStaticView extends React.Component {
 
 	render() {
 		let parsed = queryString.parse(this.props.location.search)
-		// if (this.props.LOAD_INSURANCE) {
-		// 	let parsed = queryString.parse(this.props.location.search)
-		// 	return (
-		// 		<div className="profile-body-wrap">
-		// 			<ProfileHeader showPackageStrip={true}/>
-		// 			<HelmetTags tagsData={{
-		//                   	canonicalUrl: `${CONFIG.API_BASE_URL}/insurance/insurance-plans`,
-		//                   	title: 'OPD Insurance | Buy OPD Insurance Cover | OPD Cover',
-		//                   	description: 'OPD Insurance: Buy OPD insurance cover & get cashless benefits on lab tests & doctor consultation with a network of over 15000 doctors and 2000 labs.'
-		//               	}} noIndex={false} />                
-		// 			<section className="container container-top-margin cardMainPaddingRmv">
-		// 				<div className="row no-gutters dsktp-row-gutter">
-		// 					<div className="col-12 col-md-7 col-lg-7 ins-main-padding">
-		// 						<section className="profile-book-screen" style={{position:'relative'}}>
-		// 							<div>
-		// 								{/*<div>
-		//                                   <span style={{ cursor: 'pointer' }} onClick={this.shortenUrl.bind(this)}>
-		//                                       <img src={ASSETS_BASE_URL + "/img/customer-icons/url-short.svg"} style={{ width: 80 }} />
-		//                                   </span>
-		//                               </div>*/}
-		// 								{
-		// 									this.state.shortURL ? <div className="shareLinkpopupOverlay" onClick={() => {
-		// 										this.setState({ shortURL: "" })
-		// 									}}>
-		// 										<div className="shareLinkpopup" onClick={(e) => {
-		// 											e.stopPropagation()
-		// 										}}>
-		// 											<p>{this.state.shortURL}</p>
-		// 											<CopyToClipboard text={this.state.shortURL}
-		// 												onCopy={() => {
-		// 													SnackBar.show({ pos: 'bottom-center', text: "Shortened URL Copied." });
-		// 													this.setState({ shortURL: "" })
-		// 												}}>
-		// 												<span className="shrelinkBtn">
-		// 													<button>Copy</button>
-		// 												</span>
-		// 											</CopyToClipboard>
-		// 										</div>
-		// 									</div> : ""
-		// 								}
-		// 								<InsurCommon {...this.props} isSelectPlan={true} is_checked={this.state.is_checked} />
-		// 								{/* coverage listing */}
-		// 								<div className="widget mrt-20">
-		// 									<div className="coverage-list-container border-bg-transprant">
-		// 										<table className="table table-bordered insurance-tbl insurance-checkboxes">
-		// 											<thead>
-		// 												<tr>
-		// 													<th className="tbl-first-head"><p>Coverage (1 Year)</p></th>
-		// 													<th className="tbl-second-head"><p>Annual Premium</p></th>
-		// 												</tr>
-		// 											</thead>
-		// 											<tbody>
-		// 												{
-		// 													this.props.insurnaceData['insurance'][0].plans.map((result, i) => {
-		// 														return <tr id={result.id} key={i} onClick={this.selectPlan.bind(this, result)} ref={result.adult_count == 2 && result.child_count == 2 ? (input) => { this.textInput = input } : 'ref_0'}>
-		// 															<td>
-		// 																<label className="container-radio" htmlform={i} >{result.name}
-		// 																	<input type="radio" name="gender" id={i} value={i} checked={this.state.is_checked ? this.state.is_checked === result.id : result.is_selected} />
-		// 																	<span className="doc-checkmark"></span>
-		// 																</label>
-		// 															</td>
-		// 															<td><span>₹ {result.amount}</span></td>
-		// 														</tr>
-		// 													})
-		// 												}
-		// 											</tbody>
-		// 										</table>
-		// 									</div>
-		// 								</div>
-		// 								{/* coverage listing */}										
-		// 							</div>
-		// 							<div style={{position:'absolute', bottom:'-40px', right:'15px'}}><a className="fw-500" href="/terms" style={{color:"#f78631"}} onClick={(e) => {
-		// 							e.preventDefaut();
-		// 							this.props.history.push('/terms')
-		// 						}}>Website T&C Apply</a></div>
-		// 						</section>
-
-		// 						{this.state.showPopup ?
-		// 							<InsurPopup {...this.props} selected_plan={this.state.selected_plan_data} hideLoginPopup={this.hideLoginPopup.bind(this)} isLead={this.state.isLead} closeLeadPopup={this.closeLeadPopup.bind(this)} popupClass={this.state.popupClass} overlayClass={this.state.overlayClass} identifyUserClick={this.state.identifyUserClick}/> : ''
-		// 						}
-		// 						{
-		// 							parsed.show_button?
-		// 							<div className="sticky-btn fixed insuBtnsContainer">
-		// 									<button className="insu-left-white-btn" onClick={()=>this.props.history.go(-1)}>Back to Booking
-		// 									</button>
-		// 									{
-		// 										STORAGE.checkAuth()?
-		// 										<button className="insu-right-orng-btn" onClick={this.proceedPlan.bind(this)}>Proceed {this.state.selected_plan_price} <span className="foot-btn-sub-span">{this.state.gst}</span>
-		// 										</button>
-		// 										:
-		// 										<button className="insu-right-orng-btn" id="proceedLead" onClick={this.proceedLead.bind(this, 'proceed')}>Proceed {this.state.selected_plan_price} <span className="foot-btn-sub-span">{this.state.gst}</span>
-		// 										</button>
-		// 									}
-		// 							</div>	
-		// 							:STORAGE.checkAuth()?
-		// 								<button className="v-btn p-3 v-btn-primary btn-lg fixed horizontal bottom no-round btn-lg text-lg sticky-btn" onClick={this.proceedPlan.bind(this)}>Proceed {this.state.selected_plan_price} <span className="foot-btn-sub-span">{this.state.gst}</span>
-		// 								</button>
-		// 								:
-		// 								<div className="sticky-btn fixed insuBtnsContainer">
-		// 									<button className="insu-left-white-btn" id="proceedLead" onClick={this.proceedLead.bind(this, 'interest')}>Click here to know more
-		// 									</button>
-		// 									<button className="insu-right-orng-btn" onClick={this.proceedLead.bind(this, 'proceed')}>Proceed {this.state.selected_plan_price} <span className="foot-btn-sub-span">{this.state.gst}</span>
-		// 									</button>
-		// 								</div>
-		// 						}
-		// 					</div>
-
-		// 					<ChatPanel />
-		// 				</div>
-		// 			</section>
-		// 		</div>
-		// 	)
-		// }
-		// else{
-		// 	if(this.props.insurnaceData.certificate){
-		// 		this.props.history.push('/insurance/certificate')
-		// 	}
-		// 	return(
-		// 	<div className="profile-body-wrap">
-		//        <ProfileHeader />
-		// 		<Loader />
-		// 	</div>
-		// 	)
-		// }
+		
 		if (this.props.LOAD_INSURANCE) {
 			return (
 				<div>
@@ -480,9 +359,9 @@ class InsuranceStaticView extends React.Component {
 									}
 								</div>
 							</div>
-
 						</section>
-						{this.state.showPopup ?
+						{
+							this.state.showPopup ?
 							<InsurPopup {...this.props} selected_plan={this.state.selected_plan_data} hideLoginPopup={this.hideLoginPopup.bind(this)} isLead={this.state.isLead} closeLeadPopup={this.closeLeadPopup.bind(this)} popupClass={this.state.popupClass} overlayClass={this.state.overlayClass} identifyUserClick={this.state.identifyUserClick} /> : ''
 						}
 					</div>
