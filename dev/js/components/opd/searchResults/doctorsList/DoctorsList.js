5f82e37477df5849b2432387a448814290823f0e
Kunal Batra <>
2019-07-02 16:00:12 +0530

    detect location widget on top

--- dev/js/components/opd/searchResults/doctorsList/DoctorsList.js
+++ dev/js/components/opd/searchResults/doctorsList/DoctorsList.js
@@ -307,8 +307,8 @@ class DoctorsList extends React.Component {
                                                             }
 
                                                             {
-                                                                (i == 1 && this.props.seoFriendly && this.props.match.url.includes('-sptcit') && this.props.commonSelectedCriterias && this.props.commonSelectedCriterias.length) ?
-                                                                    <div className="d-flex align-items-center justify-content-between auto-location-widget mb-3">
+                                                                (i == 0 && this.props.seoFriendly && this.props.match.url.includes('-sptcit') && this.props.commonSelectedCriterias && this.props.commonSelectedCriterias.length) ?
+                                                                    <div className="d-flex align-items-center justify-content-between auto-location-widget mb-3 mrt-20">
                                                                         <div className="d-flex align-items-center auto-location-text">
                                                                             <img src={ASSETS_BASE_URL + '/img/customer-icons/location-colored.svg'} />
                                                                             <p className="fw-500">Show {this.props.commonSelectedCriterias[0].name} near me</p>
