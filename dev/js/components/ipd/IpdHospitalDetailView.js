c3348c5b34596dfa67544e53e9a9e04c51d5691b
Mayank <mayankyadav403@gmail.com>
2019-07-16 17:26:24 +0530

    ui fixes

--- dev/js/components/ipd/IpdHospitalDetailView.js
+++ dev/js/components/ipd/IpdHospitalDetailView.js
@@ -266,7 +266,7 @@ class HospitalDetailView extends React.Component {
 							</div>
 							{
 								this.props.ipd_hospital_detail && this.props.ipd_hospital_detail.is_ipd_hospital?
-								<div id="common_search">
+								<div id="common_search" className="ipd-sl-srch">
 									<CommonSearch {...this.props} hospital_id_search={this.props.hospital_id} commonSearch={true} getInputFocus={this.getInputFocus.bind(this)} hospital_lat= {this.props.ipd_hospital_detail.lat} hospital_long = {this.props.ipd_hospital_detail.long}/>
 								</div>
 								:''
