3874a7c33e82b13a7accdfcca881e83c0dcf446b
prince kumar <princek@policybazaar.com>
2019-09-25 16:32:51 +0530

    rm

--- dev/js/components/commons/article/Article.js
+++ dev/js/components/commons/article/Article.js
@@ -462,7 +462,7 @@ class Article extends React.Component {
                                                 }
 
                                                 {
-                                                    this.state.articleData && this.state.articleData.title && this.props.match.path.split('-')[1] === 'mddp' && false?
+                                                    this.state.articleData && this.state.articleData.title && this.props.match.path.split('-')[1] === 'mddp'?
                                                         <React.Fragment>
                                                             <div className="buy-med-btn" id="medicine-btn-div">
                                                                 <button className="v-btn v-btn-primary btn-lg text-sm" id="medicine-btn" onClick={() => this.buyMedicineClick()}>Buy {this.state.articleData.title.split('|')[0]} at Flat 20% Off</button>
