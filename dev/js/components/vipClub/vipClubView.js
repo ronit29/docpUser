2e50978f45b593d9e7fb2c4b944aa1962bc77786
rishab <rishabsurana@docprime.com>
2019-09-27 11:03:49 +0530

    removed static discount line on vip page

--- dev/js/components/vipClub/vipClubView.js
+++ dev/js/components/vipClub/vipClubView.js
@@ -336,7 +336,7 @@ class VipClubView extends React.Component {
                                             <div className="vip-free-doc vip-bkdiscount-bg">
                                                 <div className="bkdiscount-text">
                                                     <h4 className="vip-card-heading">25% Discount on Lab Bookings </h4>
-                                                    <p>Maximum discount upto ₹200</p>
+                                                    {/*<p>Maximum discount upto ₹200</p>*/}
                                                 </div>
                                             </div>
                                         </div>
