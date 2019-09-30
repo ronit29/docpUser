76dcef0d39747c34e5cf05116c691104fecb0523
Mayank <mayankyadav403@gmail.com>
2019-09-30 19:21:00 +0530

    ui update

--- dev/js/components/opd/doctorProfile/vipProfilePopup.js
+++ dev/js/components/opd/doctorProfile/vipProfilePopup.js
@@ -2,7 +2,7 @@ import React from 'react'
 export default ({props,toggle}) => {
 	return(
 		<div>
-				<div className="cancel-overlay" onClick={()=>toggle()}></div>
+				<div className="cancel-overlay vip-cncl-overlay" onClick={()=>toggle()}></div>
 				<div className="widget cancel-appointment-div cancel-popup vip-pop-mbl">
 					<img src={ASSETS_BASE_URL + "/img/icons/close.png"} style={{ cursor: 'pointer', zIndex: '9' }} className="close-modal" onClick={()=>toggle()}/>
 					<div className="widget-header text-center action-screen-header" style={{ position: 'relative', paddingTop: '' }} >
