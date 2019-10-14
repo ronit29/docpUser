8154419d6cebae470c7699ab5e3f00412639624b
Mayank <mayankyadav@docprime.com>
2019-10-14 14:08:03 +0530

    icon update

--- dev/js/components/opd/doctorProfile/vipProfilePopup.js
+++ dev/js/components/opd/doctorProfile/vipProfilePopup.js
@@ -7,7 +7,7 @@ export default ({props,toggle, navigateToVip}) => {
 					<img src={ASSETS_BASE_URL + "/img/icons/close.png"} style={{ cursor: 'pointer', zIndex: '9' }} className="close-modal" onClick={()=>toggle()}/>
 					<div className="widget-header text-center action-screen-header" style={{ position: 'relative', paddingTop: '' }} >
 						<p className="vp-pop-grn">Save 70% on Appointments</p>
-						<p className="vp-pop-sub-txt"> Become Docprime the member of Docprime </p>
+						<p className="vp-pop-sub-txt"> Become Docprime <img src={ASSETS_BASE_URL + "/img/viplog.png"}/> member today </p>
 						<button className="vp-pop-ch-btn" onClick={()=>navigateToVip()}>Know more</button>
 					</div>
 				
