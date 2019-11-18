58d8657e7ef0d32c057ab0dde1d650baccf282af
Gurpreet <gurpreet@admins-MacBook-Air.local>
2019-11-18 10:29:10 +0530

    medlife tnc changes

--- dev/js/components/vipClub/vipTncView.js
+++ dev/js/components/vipClub/vipTncView.js
@@ -10,34 +10,12 @@ export default ({ toggle,props,is_insurance_applicable }) => {
                 <hr />
             </div>
             <div className="tnc-column">
-                {/*<p className="fw-500">For any online paid appointments, you can cancel your scheduled or re-booked appointment and initiate immediate refund at any time. An immediate refund shall be subject to terms and conditions as described under this section mentioned below.
-
-                    In the event, the services are not availed at the appointed date and time and our systems do not validate the URN generated on your registered mobile number, we will automatically cancel your appointment at 12:00 midnight of next date of your appointment date.
-
-                    Occasionally, appointments may be cancelled or postponed by the Third Party Service Provider. Should this occur, we will attempt to contact or inform you and you may re-schedule your appointment as per your convenience or visit www.docprime.com for fresh/re-booking on the Website.
-
-                    Cancellation through mail or call center is allowed for all the bookings until the time of appointment or 12:00 midnight of next date of your appointment date. In such cases, we will initiate an immediate refund of your money as per the process defined under Refund, Rescheduling and Cancellation section under the End User Agreement. 
-                    <u style={{color: '#f78631',cursor:'pointer',display:'inline-block'}} onClick={() =>props.history.push('/terms?forBookingScroll=true')}>click here</u>
-                </p>*/}
-                {/* <ul className="booking-cancel">
-                    
-                </ul> */}
-                {/* <ul className="qa-column">
-                    <li className="mb-24">
-                        <span><strong>What are the benefits of being a gold member?</strong></span><br/>
-                        <span>You can avail exclusive discounts on Doctor and Lab test appointments for the covered members in the plan. Additionally, you can get upto 23% discount on prescribed medicines on Medlife.com. The membership will last till the duration of the plan.</span>
-                    </li>
-                    <li className="mb-24">
-                        <span><strong>How I can avail discounts on medicines?</strong></span><br/>
-                        <span>You can visit <a href="https://www.medlife.com/">https://www.medlife.com/</a> website or mobile application and use the exclusive coupon code provided to you once you become a gold member. You can get upto 23% discount on prescription drugs. Promo code can be used multiple times for 1 year without any minimum order value. </span>
-                    </li>
-                </ul> */}
                 <h3 className="mb-24"><strong>Medlife TnC</strong></h3>
                 <h5><strong>Offer Terms:</strong></h5>
                 <ul className="offer-terms-column">
                     <li>Save Up to 23% on all prescribed medicines exclusively for <strong>Docprime Gold / VIP users</strong></li>
                     <li>No minimum order value required to avail discount.</li>
-                    <li>Visit <a href="http://bit.ly/2NXLR5u">http://bit.ly/2NXLR5u</a></li>
+                    <li>Visit <a href="https://www.medlife.com/">Medlife.com</a></li>
                     <li>Upload prescription &amp; select medicines.</li>
                     <li>Enter promo in the order checkout page. Complete payment via any mode. **Promo not applicable on OTC products</li>
                 </ul>
