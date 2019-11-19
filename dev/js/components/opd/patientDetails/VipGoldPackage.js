81d41e0c55419a314b678d4b8346cd001ed8491f
Mayank <mayankyadav@docprime.com>
2019-11-19 17:00:17 +0530

    ui update

--- dev/js/components/opd/patientDetails/VipGoldPackage.js
+++ dev/js/components/opd/patientDetails/VipGoldPackage.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-class VipGoldPackagePriceView extends React.PureComponent{
+class VipGoldPackagePriceView extends React.PureComponent {
 
-	render(){
+	render() {
 
-		return(
+		return (
 			<div className="search-el-popup-overlay cancel-overlay-zindex" onClick={(e) => {
 				e.preventDefault()
 				e.stopPropagation()
@@ -19,43 +19,45 @@ class VipGoldPackagePriceView extends React.PureComponent{
 							}}><img src={ASSETS_BASE_URL + "/img/icons/close.png"} />
 							</span>
 						</div>
-						<div className="gold-offer-cont">
-                            <h4 className="gold-ofr-hdng">LIMITED PERIOD OFFER</h4>
-                            <div className="gold-list-container">
-                                {
-                                	this.props.vipGoldPlans && this.props.vipGoldPlans.length > 0 ?
-									this.props.vipGoldPlans.map((value, key)=>{
-                                		return <div key={key} className={`gold-ofr-lising ${value.id == (this.props.selected_vip_plan && this.props.selected_vip_plan.id) ? 'gold-select' : ''}`} onClick={()=>this.props.toggleGoldPlans(value)}>
-	                                            <div className="gold-mnthplan">
-	                                                <p className="mnth-plan-gld">
-	                                                Coverage: {value.total_allowed_members} {parseInt(value.total_allowed_members)>1?'Members':'Member'}
-	                                                </p>
-	                                                <p className="gld-cvr-txt">Valid for {value.tenure} Months {value.is_selected ? <span>POPULAR</span> : ''}</p>
-	                                            </div>
-	                                            <div className="gold-price">
-	                                                <p className="gld-prc"><span className="gold-prc-cut">₹{value.mrp}</span> ₹{value.deal_price}</p>
-	                                                <div className="gold-pln-slct-radio">
-	                                                    <div className="gd-rdio-gray"></div>
-	                                                    <img className="gd-rdio-chk" src={ASSETS_BASE_URL + '/img/vip-chk.svg'} />
-	                                                </div>
-	                                            </div>
-	                                        </div>
-                                	})
-                                    : ''}
+						<h4 className="gold-pop-hdng">Select Plan</h4>
+						<div className="gold-offer-cont gold-pop-pading">
+							<h4 className="gold-ofr-hdng">LIMITED PERIOD OFFER</h4>
+							<div className="gold-list-container">
+								{
+									this.props.vipGoldPlans && this.props.vipGoldPlans.length > 0 ?
+										this.props.vipGoldPlans.map((value, key) => {
+											return <div key={key} className={`gold-ofr-lising ${value.id == (this.props.selected_vip_plan && this.props.selected_vip_plan.id) ? 'gold-select' : ''}`} onClick={() => this.props.toggleGoldPlans(value)}>
+												<div className="gold-mnthplan">
+													<p className="mnth-plan-gld">
+														Coverage: {value.total_allowed_members} {parseInt(value.total_allowed_members) > 1 ? 'Members' : 'Member'}
+													</p>
+													<p className="gld-cvr-txt">Valid for {value.tenure} Months {value.is_selected ? <span>POPULAR</span> : ''}</p>
+												</div>
+												<div className="gold-price">
+													<p className="gld-prc"><span className="gold-prc-cut">₹{value.mrp}</span> ₹{value.deal_price}</p>
+													<div className="gold-pln-slct-radio">
+														<div className="gd-rdio-gray"></div>
+														<img className="gd-rdio-chk" src={ASSETS_BASE_URL + '/img/vip-chk.svg'} />
+													</div>
+												</div>
+											</div>
+										})
+										: ''}
 
-                            </div>
-                            <p onClick={this.props.goToGoldPage}>View Details</p>
-                            {
-                                false && this.props.selected_plan_data && Object.keys(this.props.selected_plan_data).length > 0 && this.props.is_vip_gold && this.props.vipClubList && this.props.vipClubList.gold_plans && this.props.vipClubList.gold_plans.length > 0 && this.props.selected_plan_data.show_consultation_text ?
-                                    <p className="gld-lst-foot-txt">Includes Unlimited Online Consultation <span>(General
+							</div>
+
+							{
+								false && this.props.selected_plan_data && Object.keys(this.props.selected_plan_data).length > 0 && this.props.is_vip_gold && this.props.vipClubList && this.props.vipClubList.gold_plans && this.props.vipClubList.gold_plans.length > 0 && this.props.selected_plan_data.show_consultation_text ?
+									<p className="gld-lst-foot-txt">Includes Unlimited Online Consultation <span>(General
                                         Physician)</span></p>
-                                    : ''
-                            }
-                        </div>
+									: ''
+							}
+						</div>
+						<p className="gol-pop-custom-btn" onClick={this.props.goToGoldPage}>View Details</p>
 					</div>
 				</div>
 			</div>
-			)
+		)
 	}
 }
 
