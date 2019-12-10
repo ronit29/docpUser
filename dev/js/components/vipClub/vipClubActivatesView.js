4166f4b40e3a479389f0ade6a9593afe450fbf0f
“rj_rishab28” <rishabsurana@docprime.com>
2019-12-10 12:44:55 +0530

    done dev

--- dev/js/components/vipClub/vipClubActivatesView.js
+++ dev/js/components/vipClub/vipClubActivatesView.js
@@ -103,6 +103,10 @@ class VipClub extends React.Component {
         if (this.props.vip_club_db_data.data.user && Object.keys(this.props.vip_club_db_data.data.user).length > 0 && this.props.vip_club_db_data.data.user.plus_members && this.props.vip_club_db_data.data.user.plus_members.length > 0) {
             primary_user = this.props.vip_club_db_data.data.user.plus_members.filter((x => x.is_primary_user))[0]
         }
+        let is_corporate =false
+        if(this.props.vip_club_db_data.data.plan && this.props.vip_club_db_data.data.plan.length >0){
+            is_corporate = this.props.vip_club_db_data.data.plan[0].is_corporate
+        }
         return (
             <div className="profile-body-wrap" style={{ background: "" }}>
                 <div className="d-none d-lg-block">
@@ -153,7 +157,7 @@ class VipClub extends React.Component {
                                             </div>
                                         </div>
                                         : ''}
-                                    {this.props.data.is_member_allowed ?
+                                    {this.props.data.is_member_allowed && !is_corporate?
                                         <div className= {`${this.props.is_gold?'gold-white-bg-container mb-24':''}`} style={{ paddingTop: 0, paddingBottom: 20 }}>
                                             <div className="vip-act-pop text-right" style={{ display: 'block' }}>
                                                 <div className="vip-wrn-img text-left">
@@ -176,6 +180,26 @@ class VipClub extends React.Component {
                                     {
                                         !this.props.is_gold && this.props.data.plan && this.props.data.plan.length > 0 && this.props.data.plan[0].utilize && Object.keys(this.props.data.plan[0].utilize).length > 0 ?
                                             <React.Fragment>
+                                                {is_corporate?
+                                                    <div className="gold-white-bg-container mb-24">
+                                                        <h4 className="vip-card-heading">Vip Benefits</h4>
+                                                        <div className="gold-benifi-cards-cont">
+                                                            <div className="gold-benifi-cards">
+                                                                <img src={ASSETS_BASE_URL + '/img/gl1.png'} />
+                                                                <p>Book<br /> Doctors</p>
+                                                            </div>
+                                                            <div className="gold-benifi-cards">
+                                                                <img src={ASSETS_BASE_URL + '/img/gl2.png'} />
+                                                                <p>Book <br />Labs</p>
+                                                            </div>
+                                                            <div className="gold-benifi-cards">
+                                                                <img src={ASSETS_BASE_URL + '/img/medlife-med.png'} />
+                                                                <p> Order<br/> Medicines</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    :''
+                                                }
                                                 <div className="vip-dsh-main-cont mb-3">
                                                     <div className="vip-acnt-heading">
                                                         <h5>Your Account</h5>
@@ -185,9 +209,11 @@ class VipClub extends React.Component {
                                                         <div className="vip-cnslt-card">
                                                             <h5 className="vip-brder-hdng">In-Clinic Consultation</h5>
                                                             <ul>
-                                                                <li><p>Total Limit: <span>₹{this.props.data.plan[0].utilize.doctor_consult_amount}  </span></p></li>
+                                                                <li><p>Total Limit: <span>₹{this.props.data.plan[0].utilize.doctor_consult_amount >= 99999?'Unlimited':this.props.data.plan[0].utilize.doctor_consult_amount}  </span></p></li>
                                                                 <li><p>Utilized: <span>₹{this.props.data.plan[0].utilize.doctor_amount_utilized} </span></p></li>
-                                                                <li><p>Available: <span className="vip-crd-avl-grn">₹{this.props.data.plan[0].utilize.doctor_amount_available}</span></p></li>
+                                                                {this.props.data.plan[0].utilize.doctor_amount_available <=99999?
+                                                                    <li><p>Available: <span className="vip-crd-avl-grn">₹{this.props.data.plan[0].utilize.doctor_amount_available}</span></p></li>
+                                                                :''}
                                                             </ul>
                                                         </div>
                                                         <div className="vip-cnslt-card">
@@ -342,7 +368,7 @@ class VipClub extends React.Component {
                                                                 })
                                                             }
                                                             {
-                                                                this.props.data.is_member_allowed ?
+                                                                this.props.data.is_member_allowed && !is_corporate?
                                                                     <li onClick={this.AddMemberDetails.bind(this)}>
                                                                         <h4 className="vip-acrd-add-member"><img className="vip-add-img" src={ASSETS_BASE_URL + '/img/vip-mem.svg'} />Add Members</h4>
                                                                     </li>
