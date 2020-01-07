d38506ccbdd9f85c342c37aeb1143b10adc618fa
“rj_rishab28” <rishabsurana@docprime.com>
2020-01-07 13:37:15 +0530

    D

--- dev/js/components/opd/patientDetails/choosePatientNew.js
+++ dev/js/components/opd/patientDetails/choosePatientNew.js
@@ -408,7 +408,7 @@ class ChoosePatientNewView extends React.Component {
                                 (this.props.is_opd || this.props.is_lab) && !this.props.patient.dob?
                                     <div className="slt-nw-input summery-dob-cont">
                                         <label className="slt-label" htmlFor="male"><sup className="requiredAst">*</sup>Dob:
-                                        <p className="dob-input-sub">DD/MM/YYYY</p>
+                                        <p className="dob-input-sub">dd/mm/yyyy</p>
                                         </label>
                                         <NewDateSelector {...this.props} getNewDate={this.getNewDate.bind(this)} is_dob_error={this.state.is_dob_error} old_dob={this.state.dob} is_summary={true}/>
                                     </div>
@@ -473,7 +473,7 @@ class ChoosePatientNewView extends React.Component {
                                 </div>
                                 <div className="slt-nw-input summery-dob-cont">
                                     <label className="slt-label" htmlFor="male"><sup className="requiredAst">*</sup>Dob:
-                                    <p className="dob-input-sub">DD/MM/YYYY</p>
+                                    <p className="dob-input-sub">dd/mm/yyyy</p>
                                     </label>
                                     <NewDateSelector {...this.props} getNewDate={this.getNewDate.bind(this)} is_dob_error={this.state.is_dob_error} old_dob={this.state.dob} is_summary={true}/>
                                 </div>
