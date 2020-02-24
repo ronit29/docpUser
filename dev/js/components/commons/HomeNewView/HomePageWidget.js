b0d1acc352984af912ec3436686fb14621bd3d6c
Princekumar7b <prince.k@docprime.com>
2020-02-24 11:23:24 +0530

    added className

--- dev/js/components/commons/HomeNewView/HomePageWidget.js
+++ dev/js/components/commons/HomeNewView/HomePageWidget.js
@@ -64,7 +64,7 @@ class HomePageWidgets extends React.PureComponent {
                                 <svg xmlns="http://www.w3.org/2000/svg" width="19.411" height="12.941" viewBox="0 0 19.411 12.941" className="ml-2">
                                     <g transform="translate(-0.282 -87.521)">
                                         <g transform="translate(0.282 87.521)">
-                                            <path class="link-arrow-clr"
+                                            <path className="link-arrow-clr"
                                                 d="M19.293,91.517l-6.066-6.066a.4.4,0,0,0-.572.572L18.03,91.4H.4a.4.4,0,0,0,0,.809H18.03l-5.376,5.376a.4.4,0,1,0,.572.572l6.066-6.066A.4.4,0,0,0,19.293,91.517Z"
                                                 transform="translate(0 -85.333)" />
                                         </g>
@@ -164,7 +164,7 @@ class HomePageWidgets extends React.PureComponent {
                         <svg xmlns="http://www.w3.org/2000/svg" width="5.004" height="8.472" viewBox="0 0 5.004 8.472">
                             <g transform="translate(105.702) rotate(90)">
                                 <g transform="translate(0 100.698)">
-                                    <path class="link-arrow-clr"
+                                    <path className="link-arrow-clr"
                                         d="M8.336,101.116l-.278-.279a.465.465,0,0,0-.656,0L4.238,104l-3.169-3.169a.465.465,0,0,0-.655,0l-.278.278a.464.464,0,0,0,0,.655l3.773,3.787a.474.474,0,0,0,.329.149h0a.474.474,0,0,0,.328-.149l3.77-3.777a.47.47,0,0,0,0-.66Z"
                                         transform="translate(0 -100.698)" />
                                 </g>
@@ -175,7 +175,7 @@ class HomePageWidgets extends React.PureComponent {
                         <svg xmlns="http://www.w3.org/2000/svg" width="5.004" height="8.472" viewBox="0 0 5.004 8.472">
                             <g transform="translate(105.702) rotate(90)">
                                 <g transform="translate(0 100.698)">
-                                    <path class="link-arrow-clr"
+                                    <path className="link-arrow-clr"
                                         d="M8.336,101.116l-.278-.279a.465.465,0,0,0-.656,0L4.238,104l-3.169-3.169a.465.465,0,0,0-.655,0l-.278.278a.464.464,0,0,0,0,.655l3.773,3.787a.474.474,0,0,0,.329.149h0a.474.474,0,0,0,.328-.149l3.77-3.777a.47.47,0,0,0,0-.66Z"
                                         transform="translate(0 -100.698)" />
                                 </g>
