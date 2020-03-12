016c49f0e600d3d740f58860aacda3bfa69e566b
Gurpreet <sgurpreet@docprime.com>
2020-03-12 18:37:42 +0530

    continue button added

--- dev/js/components/vipClub/vipGoldView.js
+++ dev/js/components/vipClub/vipGoldView.js
@@ -669,14 +669,14 @@ class VipGoldView extends React.Component {
                             <div className="fixed p-0 v-btn  btn-lg horizontal bottom no-round text-lg buttons-addcart-container">
                                 {
                                     this.props.is_booking_page !== '' && (this.props.is_booking_page == 'opd' || this.props.is_booking_page == 'lab') ?
-                                        <button className="v-btn-primary book-btn-mrgn-adjust" style={{background:'#1b97fd',borderColor: "#1b97fd"}} onClick={this.goBack.bind(this)}>
+                                        <button className="desk-cont-btn" onClick={this.goBack.bind(this)}>
                                             <p>Continue Booking</p>
                                         </button>
                                         :this.props.is_from_organic?
-                                        <button className="v-btn-primary book-btn-mrgn-adjust" style={{background:'#1b97fd',borderColor: "#1b97fd"}} onClick={this.goBack.bind(this)}>
+                                        <button className="desk-cont-btn" onClick={this.goBack.bind(this)}>
                                             <p>Continue Booking</p>
                                         </button>
-                                        :<button className="v-btn-primary book-btn-mrgn-adjust" style={{background:'#1b97fd',borderColor: "#1b97fd"}} onClick={this.props.proceed.bind(this)}>
+                                        :<button className="v-btn-primary book-btn-mrgn-adjust mbl-cont-btn" style={{background:'#1b97fd',borderColor: "#1b97fd"}} onClick={this.props.proceed.bind(this)}>
                                             <p>Continue</p>
                                         </button>
                                 }
