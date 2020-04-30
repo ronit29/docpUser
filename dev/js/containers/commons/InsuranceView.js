aa67d1f3efb6f5353ab143ab6defdd0fb6f43ec5
Mayank <mayankyadav@docprime.com>
2020-04-30 10:48:11 +0530

    ui update

--- dev/js/containers/commons/InsuranceView.js
+++ dev/js/containers/commons/InsuranceView.js
@@ -256,9 +256,11 @@ class Insurance extends React.Component {
 												<div className="dlts-cnt">
 													<p><img className="img-fluid" src={ASSETS_BASE_URL + '/img/nw-usr.svg'} /><span>Mayank Yadav</span></p>
 													<p><img className="img-fluid" src={ASSETS_BASE_URL + '/img/calendar-01.svg'} /><span>09/02/1990</span></p>
-													<p><img className="img-fluid" src={ASSETS_BASE_URL + '/img/location-01.svg'} /><span>110092</span></p>
 													<p><img className="img-fluid" src={ASSETS_BASE_URL + '/img/mail-01.svg'} /><span>mayank@gmail.com</span></p>
+													<p><img className="img-fluid" src={ASSETS_BASE_URL + '/img/location-01.svg'} /><span>110092</span></p>
 													<p><img className="img-fluid" src={ASSETS_BASE_URL + '/img/customer-icons/location-colored.svg'} /><span>B11/1 Vinod nagar West Delhi</span></p>
+													<p><img className="img-fluid" src={ASSETS_BASE_URL + '/img/nw-usr.svg'} /><span>Nominee Name</span></p>
+													<p><img className="img-fluid" src={ASSETS_BASE_URL + '/img/nw-usr.svg'} /><span>Brother</span></p>
 												</div>
 											</div>
 										</div>
@@ -374,10 +376,6 @@ class Insurance extends React.Component {
 											<div className="payment-content-div">
 												<p className="payment-content fw-500">GST</p>
 												<p className="payment-content fw-500">&#8377; 0,0</p>
-											</div>
-											<div className="payment-content-div">
-												<p className="payment-content fw-500">Amount Payable</p>
-												<p className="payment-content fw-500">&#8377; 000</p>
 											</div>
 											<div className="payment-content-div">
 												<p className="payment-content fw-500">Policy StartDate</p>
