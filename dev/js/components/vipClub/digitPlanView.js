0a740bc53bc66642e4982ab2671dfaec1ebc5d02
“rj_rishab28” <rishabsurana@docprime.com>
2020-05-01 20:08:03 +0530

    D

--- dev/js/components/vipClub/digitPlanView.js
+++ dev/js/components/vipClub/digitPlanView.js
@@ -7,6 +7,7 @@ import HelmetTags from '../commons/HelmetTags'
 import GTM from '../../helpers/gtm'
 import STORAGE from '../../helpers/storage';
 import SnackBar from 'node-snackbar'
+import VipLoginPopup from './digitLogin.js'
 // const queryString = require('./node_modules/query-string');
 import CarouselView from '../opd/searchResults/carouselView.js'
 
@@ -15,92 +16,32 @@ class DigitPlanView extends React.Component {
         super(props)
         this.state = {
             // selected_plan_data: this.props.selected_plan ? this.props.selected_plan : '',
+            showPopup: false,
+           selected_plan_data: this.props.selected_digit_plan ? this.props.selected_digit_plan : '',
+           selected_plan_id: this.props.selected_digit_plan && Object.keys(this.props.selected_digit_plan).length ? this.props.selected_digit_plan.id:'',
+           toggleTabType: false,
         }
     }
 
     proceed() {
-        SnackBar.show({ pos: 'bottom-center', text: 'We have currently stopped Gold subscriptions for new users. Sorry for the inconvenience caused.' })
-        return;
-        let loginUser
-        let lead_data = queryString.parse(this.props.location.search)
-        let gtmData = {
-            'Category': 'ConsumerApp', 'Action': 'VipClubBuyNowClicked', 'CustomerID': GTM.getUserId() || '', 'leadid': 0, 'event': 'vip-buynow-clicked', 'selected': ''
-        }
-        GTM.sendEvent({ data: gtmData })
-            
         if (STORAGE.checkAuth()) {
-            if (this.props.USER && Object.keys(this.props.USER.profiles).length > 0 && this.props.USER.defaultProfile) {
-                loginUser = this.props.USER.profiles[this.props.USER.defaultProfile]
-                let extraParams = {}
-                if(this.props.common_utm_tags && this.props.common_utm_tags.length >0){
-                    extraParams = this.props.common_utm_tags.filter(x=>x.type == "common_xtra_tags")[0].utm_tags
-                }
-                if (Object.keys(loginUser).length > 0 && !STORAGE.isAgent()) {
-                                        
-                    // to create vip or gold member lead for matrix
-                    let visitor_info = GTM.getVisitorInfo()
-                        if(visitor_info && visitor_info.visit_id){
-                            lead_data.visit_id = visitor_info.visit_id
-                            lead_data.visitor_id = visitor_info.visitor_id
-                        }
-                    if(this.state.is_lead_enabled){
-                        /*this.setState({is_lead_enabled:false})
-                        this.props.generateVipClubLead({selectedPlan:this.props.selected_vip_plan ? this.props.selected_vip_plan.id : '', number:loginUser.phone_number, lead_data:lead_data, selectedLocation:this.props.selectedLocation, user_name:loginUser.name, extraParams:extraParams,
-                            cb: (resp) => {
-                                let LeadIdData = {
-                                'Category': 'ConsumerApp', 'Action': 'VipLeadClicked', 'CustomerID': GTM.getUserId() || '', 'leadid': resp.lead_id ? resp.lead_id : 0, 'event': 'vip-lead-clicked', 'source': lead_data.source || ''
-                                }
-                                GTM.sendEvent({ data: LeadIdData })
-                            }
-                        })
-                        setTimeout(() => {
-                            // this.setState({is_lead_enabled:true})
-                        }, 5000)*/
-                    }
-                }
-                let url = '/vip-club-member-details?isDummy=true'
-                if (lead_data.utm_source) {
-                    url += '&utm_source=' + lead_data.utm_source
-                }
-                if (lead_data.utm_term) {
-                    url += '&utm_term=' + lead_data.utm_term
-                }
-                if (lead_data.utm_campaign) {
-                    url += '&utm_campaign=' + lead_data.utm_campaign
-                }
-                if (lead_data.utm_medium) {
-                    url += '&utm_medium=' + lead_data.utm_medium
-                }
-                if (lead_data.is_agent) {
-                    url += '&is_agent=' + lead_data.is_agent
-                }
-                if (lead_data.is_gold) {
-                    url += '&is_gold=' + lead_data.is_gold
-                }
-                this.props.history.push(url)
-                // this.props.history.push('/vip-club-member-details')
-            }
-        } else {
-            this.props.citiesData()
+            let url  = '/covid-form'
+            this.props.history.push(url)
+        }else{
             this.setState({ showPopup: true })
         }
     }
+
+    selectPlan(plan) {
+        this.setState({selected_plan_data:plan,selected_plan_id:plan.id})
+        this.props.selectDigitPlan(plan)
+    }
+    hideLoginPopup() {
+        this.setState({ showPopup: false })
+    }
     render() {
         let self = this
-        let is_gold_selected = false
-        // let selected_gold_plan_price 
-        //     {
-        //         this.props.selected_plan_data && Object.keys(this.props.selected_plan_data).length > 0 && this.props.vipClubList.gold_plans && this.props.vipClubList.gold_plans.length?
-        //         Object.entries(this.props.vipClubList.gold_plans).map(function ([key, value]) {
-        //             if(value.is_selected){
-        //                 selected_gold_plan_price = value.deal_price
-        //             }
-        //             if(parseInt(value.id) == parseInt(self.props.selected_plan_data.id)){
-        //                 is_gold_selected = true
-        //             }
-        //         })
-        //         :''
-        //     }
+        
 
         return (
             // && Object.keys(this.props.vipClubList).length > 0 && this.props.selected_plan_data &&
@@ -110,6 +51,10 @@ class DigitPlanView extends React.Component {
                     <div className="profile-body-wrap">
                     <ProfileHeader showPackageStrip={true} />
                         <section className="container article-container bottomMargin">
+                        {
+                        this.state.showPopup ?
+                            <VipLoginPopup {...this.props} selected_plan={this.state.selected_plan_data} hideLoginPopup={this.hideLoginPopup.bind(this)} closeLeadPopup={this.hideLoginPopup.bind(this)} /> : ''
+                        }
                             <div className="row main-row parent-section-row justify-content-center">
                                 <div className="col-12 col-md-10 col-lg-10 center-column">
                                     <div className="container-fluid mt-20">
@@ -155,9 +100,9 @@ class DigitPlanView extends React.Component {
                                                         {this.props.plans.map(plan=>
                                                             <tr key={plan.id}>
                                                                 <td>
-                                                                    <div className="dtl-radio">
+                                                                    <div className="dtl-radio" onClick={()=>self.selectPlan(plan,self)}>
                                                                         <label className="container-radio">{plan.name}
-                                                                            <input type="radio" />
+                                                                            <input type="radio" checked={this.state.selected_plan_id == plan.id}/>
                                                                             <span className="doc-checkmark"></span>
                                                                         </label>
                                                                     </div>
@@ -169,13 +114,6 @@ class DigitPlanView extends React.Component {
                                                             
                                                     </tbody>
                                                 </table>
-                                            
-                                            {/* <div className="term-cont-digi">
-                                                <label className="ck-bx" htmlform="test11" style={{ 'fontWeight': '500', 'fontSize': '13px' }}>
-                                                    <input type="checkbox" defaultChecked className="ins-chk-bx" value="" id="test11" name="fruit-1" />
-                                                    <span className="checkmark"></span>I Agree to the </label>
-                                                <p onClick={this.openPopup}>Terms and Conditions</p>
-                                            </div> */}
                                         </div>
                                             <div className="bottomMargin"></div>
                                         </div>
@@ -185,12 +123,12 @@ class DigitPlanView extends React.Component {
                             </div>
                             {/* ==================== Common button ==================== */}
                             <div className="sticky-btn fixed insuBtnsContainer">
-                                <button onClick={this.state.handlePlanSelect} className="insu-right-orng-btn ins-buy-btn">Proceed</button>
+                                <button onClick={()=>this.proceed()} className="insu-right-orng-btn ins-buy-btn">Proceed</button>
                             </div>
                             {/* ==================== Common button ==================== */}
                         </section>
                     </div>
-                </div >
+                </div>
             </React.Fragment>
             : <div></div>
         );
